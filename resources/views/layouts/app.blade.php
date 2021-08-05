102f47b1bcd9eb157e38400f28a7e7e486cb8e18
neeil1990 <neeil@mail.ru>
2021-08-05 12:35:19 +0300

    #122921 logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <img src="/img/logo.svg" alt="RedBox" class="brand-image">
         </a>
 
         <!-- Sidebar -->
