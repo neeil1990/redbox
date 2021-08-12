d07552aa1610726741222f420a23b82bc307cb96
neeil1990 <neeil@mail.ru>
2021-08-12 15:56:49 +0300

    no message

--- app/User.php
+++ app/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * Delete no verify users
      * @var int
      */
-    protected $delete = 30;
+    protected $delete = 20;
 
     /**
      * Send the email verification notification.
