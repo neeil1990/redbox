0026e90380420a6db64b38f6f49856f707292d68
neeil1990 <neeil@mail.ru>
2021-08-20 09:29:42 +0300

    fix

--- config/pages.php
+++ config/pages.php
@@ -13,12 +13,12 @@ return [
 
     'link' => [
         ['id' => 1, 'method' => 'keywordGenerator', 'url' => 'keyword-generator', 'name' => 'pages.keyword'],
-        ['id' => 2, 'method' => 'passwordGenerator', 'url' => 'password-generator', 'name' => 'pages.password'],
-        ['id' => 2, 'method' => 'countingTextLength', 'url' => 'counting-text-length', 'name' => 'pages.length'],
-        ['id' => 3, 'method' => 'duplicates', 'url' => 'duplicates', 'name' => 'pages.duplicates'],
-        ['id' => 4, 'method' => 'utmMarks', 'url' => 'utm-marks', 'name' => 'pages.utm'],
-        ['id' => 5, 'method' => 'roiCalculator', 'url' => 'roi-calculator', 'name' => 'pages.roi'],
-        ['id' => 6, 'method' => 'httpHeaders', 'url' => 'http-headers/{url?}', 'name' => 'pages.headers'],
+        ['id' => 2, 'method' => 'duplicates', 'url' => 'duplicates', 'name' => 'pages.duplicates'],
+        ['id' => 3, 'method' => 'utmMarks', 'url' => 'utm-marks', 'name' => 'pages.utm'],
+        ['id' => 4, 'method' => 'roiCalculator', 'url' => 'roi-calculator', 'name' => 'pages.roi'],
+        ['id' => 5, 'method' => 'httpHeaders', 'url' => 'http-headers/{url?}', 'name' => 'pages.headers'],
+        ['id' => 6, 'method' => 'passwordGenerator', 'url' => 'password-generator', 'name' => 'pages.password'],
+        ['id' => 7, 'method' => 'countingTextLength', 'url' => 'counting-text-length', 'name' => 'pages.length'],
     ],
 
 ];
