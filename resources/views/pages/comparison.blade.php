00d2bcd1d1442e6c27fc141e09f26d6cb42f863a
Nikolay <tifilum74@mail.ru>
2021-08-23 16:40:15 +0300

    #122346 added localization

--- resources/views/pages/comparison.blade.php
+++ resources/views/pages/comparison.blade.php
@@ -4,86 +4,84 @@
         @csrf
         <div class="row">
             <div class="col-sm-12 textpart">
-                <h3>Сравните списки ключевых фраз и получите общий уникальный список.</h3>
+                <h3>{{__('Compare the lists of keywords and get a common unique list.')}}</h3>
             </div>
         </div>
         <div class="row mt-3 mb-3">
             <div class="col-sm-6 d-flex flex-column">
-                <label class="font-weight-light">Первый список</label>
+                <label class="font-weight-light">{{__('First list')}}</label>
                 <textarea class="form-control" name="firstList" rows="7"
                           required>{{\Illuminate\Support\Facades\Input::old('firstList')}}</textarea>
             </div>
             <div class="col-sm-6 d-flex flex-column">
-                <label class="font-weight-light">Второй список</label>
+                <label class="font-weight-light">{{__('Second list')}}</label>
                 <textarea class="form-control" name="secondList" rows="7"
                           required>{{\Illuminate\Support\Facades\Input::old('secondList')}}</textarea>
             </div>
         </div>
         <div class="row">
             <div class="col-8 d-flex flex-column">
-                <label class="mt-3 mb-3">Тип сравнения:</label>
-                <label title="список ключевых фраз, которые нашлись как в первом, так и во втором списке (пересечение)"
-                       class="radio font-weight-light">
+                <label class="mt-3 mb-3">{{__('Comparison type:')}}</label>
+                <label
+                    title="{{__('a list of keywords that were found in both the first and second list (intersection)')}}"
+                    class="radio font-weight-light">
                     <input type="radio" name="option" value="unique" id="first-radio-option" checked
-                           onclick="saveOptionState('first')"> Уникальные фразы,
-                    которые есть в каждом из
-                    двух
-                    списков
+                           onclick="saveOptionState('first')">{{__('Unique phrases that are in each of the two lists')}}
                     <a href=""
-                       title="список ключевых фраз, которые нашлись как в первом, так и во втором списке (пересечение)"
+                       title="{{__('a list of keywords that were found in both the first and second list (intersection)')}}"
                        class="help-link">
                         <i aria-hidden="true" class="fa fa-question-circle"></i>
                     </a>
                 </label>
-                <label title="список ключевых фраз, которые нашлись в любом из списков (объединение)"
+                <label title="{{__('a list of keywords that were found in any of the lists (combining)')}}"
                        class="radio font-weight-light">
                     <input type="radio" name="option" value="union" id="second-radio-option"
-                           onclick="saveOptionState('second')"> Уникальные фразы, которые
-                    есть в любом из двух
-                    списков
-                    <a href="" title="список ключевых фраз, которые нашлись в любом из списков (объединение)"
+                           onclick="saveOptionState('second')">{{__('Unique phrases that are in either of the two lists')}}
+                    <a href="" title="{{__('a list of keywords that were found in any of the lists (combining)')}}"
                        class="help-link">
                         <i aria-hidden="true" class="fa fa-question-circle"></i>
                     </a>
                 </label>
-                <label title="список ключевых фраз, которые есть в первом списке, но нет во втором"
+                <label title="{{__('a list of keywords that are in the first list, but not in the second')}}"
                        class="radio font-weight-light">
                     <input type="radio" name="option" value="uniqueInFirstList" id="third-radio-option"
                            onclick="saveOptionState('third')">
-                    Уникальные фразы, которые есть только в первом списке
-                    <a href="" title="список ключевых фраз, которые есть в первом списке, но нет во втором"
+                    {{__('Unique phrases that are only in the first list')}}
+                    <a href="" title="{{__('a list of keywords that are in the first list, but not in the second')}}"
                        class="help-link">
                         <i aria-hidden="true" class="fa fa-question-circle"></i>
                     </a>
                 </label>
-                <label title="список ключевых фраз, которые есть во втором списке, но нет в первом"
+                <label title="a list of keywords that are in the second list, but not in the first"
                        class="radio font-weight-light">
                     <input type="radio" name="option" value="uniqueInSecondList" id="fourth-radio-option"
                            onclick="saveOptionState('fourth')">
-                    Уникальные фразы, которые есть только во втором списке
-                    <a href="" title="список ключевых фраз, которые есть во втором списке, но нет в первом"
+                    {{__('Unique phrases that are only in the second list')}}
+                    <a href="" title="a list of keywords that are in the second list, but not in the first"
                        class="help-link">
                         <i aria-hidden="true" class="fa fa-question-circle"></i>
                     </a>
                 </label>
             </div>
             <div class="col-sm-8 mt-3 mb-3 mt-3">
-                <input class="btn btn-secondary" type="submit" value="Обработать">
+                <input class="btn btn-secondary" type="submit" value="{{__('Processing')}}">
             </div>
         </div>
     </form>
     @if (\Illuminate\Support\Facades\Session::has('result'))
-        <div class="result">
+        <div class="result mt-3">
             <textarea name="result" id="comparison-result" class="form-control"
                       rows="10">{{\Illuminate\Support\Facades\Session::get('result')}}</textarea>
             <div class="col-sm-12 d-flex">
-                <button onclick="saveOfBuffer()" class="btn btn-default mt-2 mr-2">
-                    <i aria-hidden="true" class="fa fa-clipboard"></i>
-                </button>
+                <a title="{{__('Copy result')}}">
+                    <button onclick="saveOfBuffer()" class="btn btn-default mt-2 mr-2">
+                        <i aria-hidden="true" class="fa fa-clipboard"></i>
+                    </button>
+                </a>
                 <form action="{{route('download-comparison-file')}}" method="GET">
                     @csrf
                     <input type="hidden" value="{{\Illuminate\Support\Facades\Session::get('result')}}" name="result">
-                    <a title="Загрузить в виде файла" class="pull-left ml-2 mr-2">
+                    <a title="{{__('Download file')}}" class="pull-left ml-2 mr-2">
                         <button class="btn btn-default mt-2">
                             <i aria-hidden="true" class="fa fa-download"></i>
                         </button>
