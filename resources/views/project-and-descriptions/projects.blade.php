b384e7f5104cc742f49a903383389e48df550ab8
Nikolay <tifilum74@mail.ru>
2021-09-06 12:35:21 +0300

    #122350 added accordion styles

--- resources/views/project-and-descriptions/projects.blade.php
+++ resources/views/project-and-descriptions/projects.blade.php
@@ -20,7 +20,6 @@
             <div class="modal fade" id="remove-project-id-{{$project->id}}"
                  tabindex="-1"
                  role="dialog"
-                 aria-labelledby="exampleModalLabel"
                  aria-hidden="true">
                 <div class="modal-dialog w-25" role="document">
                     <div class="modal-content">
@@ -38,57 +37,94 @@
                 </div>
             </div>
             <div class="card card-default card-outline">
-                <a class="d-block w-100 collapsed" data-toggle="collapse" href="#project-id-{{$project->id}}"
-                   aria-expanded="false">
+                <div class="w-100 collapsed">
                     <div class="card-header">
                         <h4 class="card-title w-100 d-flex flex-column">
-                            <p>{{ $project->project_name }}</p>
+                            <span class="d-flex justify-content-between pb-2">
+                                <a data-toggle="collapse" class="subject_project_name"
+                                   href="#project-id-{{$project->id}}"
+                                   aria-expanded="false">
+                                {{ $project->project_name }}
+                                </a>
+                                <a data-toggle="collapse" href="#project-id-{{$project->id}}"
+                                   aria-expanded="false">
+                                <i class="fa fa-eye"></i>
+                                </a>
+                            </span>
                             <p class="short_description_project">
                                 {{ $project->short_description }}
                             </p>
                         </h4>
                     </div>
-                </a>
+                </div>
                 <div id="project-id-{{$project->id}}" class="collapse" data-parent="#accordion" style="">
                     <div class="card-body">
                         @foreach($project->descriptions as $description)
-                            <div class="modal fade" id="remove-description-id-{{$description->id}}" tabindex="-1"
-                                 role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog w-25" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-body">
-                                            <p>{{__('Delete a description')}}</p>
-                                            <p>{{__('Are you sure?')}}</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <a href="{{ route('delete.description', $description->id) }}"
-                                               class="btn btn-secondary">
-                                                {{__('Delete a description')}}
-                                            </a>
-                                            <button type="button"
-                                                    class="btn btn-default"
-                                                    data-dismiss="modal">
-                                                {{__('Back')}}
-                                            </button>
+                            <div class="accordion" id="accordion{{$description->id}}">
+                                <div class="modal fade"
+                                     id="remove-description-id-{{$description->id}}"
+                                     tabindex="-1"
+                                     role="dialog"
+                                     aria-hidden="true">
+                                    <div class="modal-dialog w-25" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-body">
+                                                <p>{{__('Delete a description')}}</p>
+                                                <p>{{__('Are you sure?')}}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <a href="{{ route('delete.description', $description->id) }}"
+                                                   class="btn btn-secondary">
+                                                    {{__('Delete a description')}}
+                                                </a>
+                                                <button type="button"
+                                                        class="btn btn-default"
+                                                        data-dismiss="modal">
+                                                    {{__('Back')}}
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="d-flex border-top">
-                                <div class="project_description col-10 mr-auto pl-0">
-                                    <p>{!! $description->description !!}</p>
-                                </div>
-                                <div class="d-flex align-items-start pt-4">
-                                    <button class="btn btn-tool">
-                                        <a href="{{ route('edit.description', $description->id) }}">
-                                            <i class="fa fa-edit"></i>
-                                        </a>
-                                    </button>
-                                    <button class="btn btn-tool">
-                                        <i class="fa fa-trash"
-                                           data-toggle="modal"
-                                           data-target="#remove-description-id-{{$description->id}}"></i>
-                                    </button>
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h5 class="mb-0">
+                                            <div class="d-flex justify-content-between">
+                                                <span class="font-weight-lighter pointer"
+                                                      data-toggle="collapse"
+                                                      data-target="#description-id-{{$description->id}}"
+                                                      aria-expanded="false"
+                                                      aria-controls="description-id-{{$description->id}}">
+                                                    {{\Illuminate\Support\Str::limit(strip_tags($description->description),50)}}
+                                                </span>
+                                                <div>
+                                                    <a href="{{ route('edit.description', $description->id) }}">
+                                                        <i class="fa fa-edit"></i>
+                                                    </a>
+                                                    <i class="fa fa-trash mr-2 ml-2"
+                                                       data-toggle="modal"
+                                                       data-target="#remove-description-id-{{$description->id}}"></i>
+                                                    <i class="fa fa-eye"
+                                                       data-toggle="collapse"
+                                                       data-target="#description-id-{{$description->id}}"
+                                                       aria-expanded="false"
+                                                       aria-controls="description-id-{{$description->id}}"></i>
+                                                </div>
+                                            </div>
+                                        </h5>
+                                    </div>
+
+                                    <div id="description-id-{{$description->id}}" class="collapse"
+                                         aria-labelledby="headingOne" data-parent="#accordion{{$description->id}}"
+                                         style="">
+                                        <div class="d-flex border-top">
+                                            <div class="project_description mr-auto pl-4 pr-4">
+                                                <p>{!! $description->description !!}</p>
+                                            </div>
+                                            <div class="d-flex align-items-start pt-4">
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         @endforeach
