cebaaf8d9ec2c839de70e60fbeda17ecc16fcecc
Nikolay <tifilum74@mail.ru>
2021-09-10 15:28:13 +0300

    #120302 changed check time

--- app/Http/Middleware/LastOnline.php
+++ app/Http/Middleware/LastOnline.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 namespace App\Http\Middleware;
+
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -14,13 +15,12 @@ class LastOnline
         if (auth()->guest()) {
             return $next($request);
         }
-        $user = Auth::user();
 
-        if ($user->last_online_at->diffInHours(now()) !== 0)
-        {
-            DB::table("users")
-                ->where("id", Auth::id())
-                ->update(["last_online_at" => now()]);
+        $user = Auth::user();
+        if ($user->last_online_at->diffInMinutes(now()) >= 10) {
+            DB::table('users')
+                ->where('id', Auth::id())
+                ->update(['last_online_at' => now()]);
         }
 
         return $next($request);
