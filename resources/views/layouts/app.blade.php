239f81da33ee0cbd2dc0f8e1adbaa9857bb1cd65
Nikolay <tifilum74@mail.ru>
2021-09-29 22:43:10 +0300

    changed padding top in app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
         <!-- Main content -->
-        <div class="content pt-3" id="app">
+        <div class="content pt-3 pb-3" id="app">
             <div class="container-fluid">
                 @yield('content')
             </div>
