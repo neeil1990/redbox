7cbc803d1692dd287456ac641f6cd140340fd0db
neeil1990 <neeil@mail.ru>
2021-09-30 11:26:50 +0300

    #122865 icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -126,6 +126,9 @@
 
 @yield('js')
 
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 @include('flash::message')
 </body>
 </html>
--- resources/views/navigation/sidebar.blade.php
+++ resources/views/navigation/sidebar.blade.php
@@ -3,86 +3,86 @@
         <li class="nav-header">{{ __('Menu') }}</li>
         <li class="nav-item">
             <a href="/" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Home') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('users.index') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="people-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Users') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('behavior.index') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Behavior') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.keyword') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="text-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Keyword generator') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.password') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Password generator') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.length') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Counting text length') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('list.comparison') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('List comparison') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('unique.words') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{__("Highlighting unique words in the text")}}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('HTML.editor') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="home-outline" class="nav-icon"></ion-icon>
                 <p>{{__('HTML editor')}}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.duplicates') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="copy-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Remove Duplicates') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.utm') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="bookmarks-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('UTM Marks') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.roi') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="calculator-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('ROI calculator') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="{{ route('pages.headers') }}" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="code-slash-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Http headers') }}</p>
             </a>
         </li>
         <li class="nav-item">
             <a href="https://docs.google.com/document/d/1TpjCQjXYV_ZWyxD-c8plld7m2-dce4fSGuMz0fgytK4/edit"
                target="_blank" class="nav-link">
-                <i class="nav-icon far fa-image"></i>
+                <ion-icon name="document-text-outline" class="nav-icon"></ion-icon>
                 <p>{{ __('Documentation') }}</p>
             </a>
         </li>
