186ac983316fddd1583873e8571cf626a2bc81cd
Nikolay <tifilum74@mail.ru>
2021-10-02 18:57:42 +0300

    #122864 changed localization

--- resources/views/main-projects/create.blade.php
+++ resources/views/main-projects/create.blade.php
@@ -1,8 +1,11 @@
 @component('component.card', ['title' => __('Create project')])
 @section('content')
-    <h4 class="pt-2 pb-2">This module allows you to create services that are displayed on the main page</h4>
+    <h3 class="pt-2 pb-2">{{ __('This module allows you to create services that are displayed on the main page') }}</h3>
+    <span
+        class="text-info"> {{ __('When you create a project, you need to manually add the localization text to the item') }}</span>
+    <p class="text-info">######################## Main page ########################</p>
     {!! Form::open(['action' =>'DescriptionProjectForAdminController@store', 'method' => 'POST'])!!}
-    <div class="col-md-6">
+    <div class="col-md-6 mt-3">
         <div class="form-group required">
             {!! Form::label("title") !!}
             {!! Form::text("title", null ,["class"=>"form-control","required"=>"required"]) !!}
@@ -16,7 +19,8 @@
             {!! Form::text("link", null ,["class"=>"form-control","required"=>"required"]) !!}
         </div>
         <div class="well well-sm clearfix">
-            <button class="btn btn-success pull-right" title="Save" type="submit">Create</button>
+            <button class="btn btn-secondary pull-right" title="Save" type="submit">{{ __('Create') }}</button>
+            <a href="http://redbox/public/main-projects" class="btn btn-default"> {{ __('Back') }}</a>
         </div>
     </div>
     {!! Form::close() !!}
