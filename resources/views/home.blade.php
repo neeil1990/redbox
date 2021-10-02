27392e541575b268bbed3775640f2615ffdabd98
Nikolay <tifilum74@mail.ru>
2021-10-02 14:14:26 +0300

    added lozalization and changed main page styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,13 @@
-@extends('layouts.app')
+@component('component.card', ['title' => __('Main page')])
 @section('content')
     @slot('css')
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-              integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-              crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.12/datatables.min.css"/>
-        <link rel="stylesheet" type="text/css"
-              href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+        <link rel="stylesheet" type="text/css" href="{{ asset('plugins/home/css/style.css') }}"/>
     @endslot
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <body>
     <div id="tablecontents" class="row p-0 m-0">
         @foreach($posts as $post)
-            <div class="card col-4 p-0 ml-auto mr-auto" data-id="{{ $post->id }}">
+            <div class="card col-4 p-0" data-id="{{ $post->id }}">
                 <div class="card-header">
                     <h5 class="card-title">{{ $post->title }}</h5>
                 </div>
@@ -63,3 +58,4 @@
         </script>
     @endslot
 @endsection
+@endcomponent
