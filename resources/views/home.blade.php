394e4dbae96f28078b7229f6e2af7e19257c0438
Nikolay <tifilum74@mail.ru>
2021-10-03 14:58:17 +0300

    #122864 changed main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,11 @@
         <link rel="stylesheet" type="text/css" href="{{ asset('plugins/home/css/style.css') }}"/>
     @endslot
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <body>
     <h4 class="pl-2">{{ __('On this page there are our services that you can use.') }}</h4>
     <p class="text-muted pl-2"> {{ __('You can drag and drop services, thereby setting up a convenient order for you') }}</p>
-    <div id="tablecontents" class="row p-0 pl-2">
+    <div id="tablecontents" class="row d-flex justify-content-center pt-3">
         @foreach($result as $item)
-            <div class="card p-0 mr-2 ml-2" data-id="{{ $item['id'] }}">
+            <div class="card p-0 mr-3 ml-3" data-id="{{ $item['id'] }}">
                 <div class="card-header d-flex w-100">
                     <h5 class="card-title w-75">{{ __($item['title']) }}</h5>
                     <span class="handle ui-sortable-handle w-25 text-right">
