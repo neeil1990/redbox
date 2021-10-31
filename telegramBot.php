4c169400a875f7caa9c8fae49b98b59b4c1ab070
Nikolay <tifilum74@mail.ru>
2021-11-01 02:19:24 +0300

    configurate telegram bot

--- telegramBot.php
+++ telegramBot.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    file_put_contents(__DIR__ . '/log.txt', file_put_contents('php://input'));
+    file_put_contents(__DIR__ . '/log.txt', file_put_contents('php://input'), FILE_APPEND);
 } catch (Exception $exception) {
     \Illuminate\Support\Facades\Log::debug('tberror', [$exception]);
 }
