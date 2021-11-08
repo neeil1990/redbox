d942ed7c7659a240dd3350a068a430fbc97e7c2d
neeil1990 <neeil@mail.ru>
2021-11-08 14:23:40 +0300

    #122357 tags

--- resources/views/meta-tags/index.blade.php
+++ resources/views/meta-tags/index.blade.php
@@ -1,7 +1,29 @@
 @component('component.card', ['title' => __('Meta tags')])
 
     @slot('css')
+        <style>
+            a.accordion-title {
+                color: #212529;
+                font-size: 1rem;
+            }
 
+            .card-header-accordion:hover {
+                background-color: rgba(0,0,0,.075);
+            }
+
+            [data-toggle=collapse] i.expandable-accordion-caret {
+                transition: -webkit-transform .3s linear;
+                transition: transform .3s linear;
+                transition: transform .3s linear,-webkit-transform .3s linear;
+            }
+
+            [data-toggle=collapse][aria-expanded=true] i.expandable-accordion-caret[class*=right] {
+                -webkit-transform: rotate(
+                        90deg);
+                transform: rotate(
+                        90deg);
+            }
+        </style>
     @endslot
 
     <meta-tags></meta-tags>
