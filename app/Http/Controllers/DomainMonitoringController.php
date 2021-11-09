06272e7efad41fe56a6e333e8dfcf9d86856c64c
Nikolay <tifilum74@mail.ru>
2021-11-09 16:19:43 +0300

    async test

--- app/Http/Controllers/DomainMonitoringController.php
+++ app/Http/Controllers/DomainMonitoringController.php
@@ -78,12 +78,12 @@ class DomainMonitoringController extends Controller
     public function checkLinkCrone($timing)
     {
         $command =
-            "php " . base_path('artisan') . " httpCheck {$timing} 0 & " .
-            "php " . base_path('artisan') . " httpCheck {$timing} 1 & " .
-            "php " . base_path('artisan') . " httpCheck {$timing} 2 & " .
-            "php " . base_path('artisan') . " httpCheck {$timing} 3 & " .
-            "php " . base_path('artisan') . " httpCheck {$timing} 4 & " .
-            "php " . base_path('artisan') . " httpCheck {$timing} 5 & " ;
+            "php ../../../artisan httpCheck {$timing} 0 & " .
+            "php ../../../artisan httpCheck {$timing} 1 & " .
+            "php ../../../artisan httpCheck {$timing} 2 & " .
+            "php ../../../artisan httpCheck {$timing} 3 & " .
+            "php ../../../artisan httpCheck {$timing} 4 & " .
+            "php ../../../artisan httpCheck {$timing} 5 & " ;
         $shell = exec($command);
         VarDumper::dump(base_path('artisan'));
     }
