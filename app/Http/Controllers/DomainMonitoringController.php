70bf3d5c236fe8ae152103a2bf85a58d1ded7159
Nikolay <tifilum74@mail.ru>
2021-11-09 15:28:29 +0300

    cleanup code

--- app/Http/Controllers/DomainMonitoringController.php
+++ app/Http/Controllers/DomainMonitoringController.php
@@ -3,16 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\DomainMonitoring;
-use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Str;
-use Spatie\Async\Pool;
 
 class DomainMonitoringController extends Controller
 {
