687b8c1e33f5579231f323c799cfbc311a474e5e
Nikolay <tifilum74@mail.ru>
2021-11-12 18:55:50 +0300

    #120303 added log

--- app/DomainMonitoring.php
+++ app/DomainMonitoring.php
@@ -181,7 +181,7 @@ class DomainMonitoring extends Model
             $project->status = 'Keyword not found';
             $project->broken = true;
             Log::debug('keyword not found', [$project]);
-            Log::debug('curl', [$curl]);
+            Log::debug('curl', [$body]);
         }
     }
 }
