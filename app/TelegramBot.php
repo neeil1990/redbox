ff03d05747a23fd8560d9601f06f0a3ece0e751a
Nikolay <tifilum74@mail.ru>
2021-11-15 16:14:36 +0300

    #120303 changed bot message

--- app/TelegramBot.php
+++ app/TelegramBot.php
@@ -98,14 +98,14 @@ class TelegramBot extends Model
 " . __('Condition:') . " <code>$project->status</code>
 " . __('Current uptime:') . " <code>$uptimePercent%</code>
 " . __('Total time of the last breakdown:') . " <code>$project->total_time_last_breakdown</code> минут
-" . __('Go to the project:') . "
+" . __('Go to the website') . "
 <a href='$link' target='_blank'>" . $link . "</a>
 " . __('Go to the service:') . "
 <a href='https://lk.redbox.su/domain-monitoring' target='_blank'>https://lk.redbox.su/domain-monitoring</a>";
         } else {
             $text = __('Project') . "  <code>$project->project_name</code>  " . __($status) . "
 " . __('Check time:') . " <code>$project->last_check</code>
-" . __('http code') . " <code>$project->code</code>
+" . __('http code:') . " <code>$project->code</code>
 " . __('Condition:') . " <code>$project->status</code>
 " . __('Current uptime:') . " <code>$uptimePercent%</code>
 " . __('Go to the project:') . "
