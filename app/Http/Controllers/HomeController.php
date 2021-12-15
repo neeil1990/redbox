2a4fc0aa3f0c6d002006c7e80537e23f4cd235a7
unknown <Nikolay>
2021-12-15 13:28:09 +0300

    #132404 changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,6 +22,34 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $projectsPositions = ProjectsPositions::where('user_id', '=', Auth::id())->get('menu_positions')->toArray();
+        if (empty($projectsPositions[0]['menu_positions'])) {
+            $result = DescriptionProject::all(['id', 'title', 'description', 'link'])->toArray();
+            for ($i = 0; $i < count($result); $i++) {
+                $result[$i]['title'] = __($result[$i]['title']);
+            }
+        } else {
+            $projectsPositions = explode(',', substr($projectsPositions[0]['menu_positions'], 0, -1));
+            $projects = DescriptionProject::all(['id', 'title', 'description', 'link'])->toArray();
+            $result = [];
+
+            foreach ($projectsPositions as $projectsPosition) {
+                foreach ($projects as $project) {
+                    if ($project['id'] === (integer)$projectsPosition) {
+                        $result[] = $project;
+                    }
+                }
+            }
+            $result = array_merge($result, $projects);
+            $result = array_unique($result, SORT_REGULAR);
+            for ($i = 0; $i < count($result); $i++) {
+                Log::debug('af un', [$result[$i]['title']]);
+                $result[$i]['title'] = __($result[$i]['title']);
+            }
+        }
+
+        dd($result);
+        return response((array)$result);
         $user_id = Auth::id();
         $projectsPositions = ProjectsPositions::where('user_id', '=', $user_id)->get('projects_positions');
         if (empty($projectsPositions[0])) {
