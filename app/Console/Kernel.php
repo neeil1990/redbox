d32605c9b9c34495845e1137115ef29ad65e2f71
neeil1990 <neeil@mail.ru>
2021-12-23 12:59:58 +0300

    #122357 fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,14 +28,14 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // Delete histories > 90 days
-        $schedule->call(new MetaTagsHistoriesDelete())->cron('0 */24 * * *');
+        $schedule->call(new MetaTagsHistoriesDelete())->cron('0 */23 * * *');
 
         //test call as 6
         //$schedule->call(new MetaTags(6))->cron('* * * * *');
 
-        $schedule->call(new MetaTags(6))->cron('0 */6 * * *');
-        $schedule->call(new MetaTags(12))->cron('0 */12 * * *');
-        $schedule->call(new MetaTags(24))->cron('0 */24 * * *');
+        $schedule->call(new MetaTags(6))->cron('0 */5 * * *');
+        $schedule->call(new MetaTags(12))->cron('0 */11 * * *');
+        $schedule->call(new MetaTags(24))->cron('0 */23 * * *');
 
         // $schedule->command('inspire')
         //          ->hourly();
