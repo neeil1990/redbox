d50dcb210bd767bbcbfc95963ac5972ca6ffea6f
neeil1990 <neeil@mail.ru>
2022-01-17 16:26:53 +0300

    #133326 fix

--- resources/views/pages/duplicates.blade.php
+++ resources/views/pages/duplicates.blade.php
@@ -1,6 +1,6 @@
 @component('component.card', ['title' => __('Remove Duplicates')])
 
-    @can('permission:Duplicates')
+    @can('Duplicates')
     <remove-duplicates :names="{{ $options }}"
                        start="{{ __('remove characters at the beginning of a word') }}: +-!"
                        end="{{ __('remove characters at the end of a word') }}: .!?"
