c9a8a01ede0035df2b4857a79f59c23d3c3f85b6
Nikolay <reddeveloper@gmail.com>
2022-02-06 18:59:49 +0300

    fix visual bugs

--- resources/views/users/panel.blade.php
+++ resources/views/users/panel.blade.php
@@ -1,9 +1,9 @@
 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-    <div class="image">
+    <div class="image" style="padding-left: 0.4rem !important;">
         @if(isset($user->image))
-            <img src="{{ Storage::url($user->image) }}" class="img-circle elevation-2" style="padding-left: 0.4rem !important;" alt="{{ $user->name }}">
+            <img src="{{ Storage::url($user->image) }}" class="img-circle elevation-2" alt="{{ $user->name }}">
         @else
-            <img src="img\profile-image.jpg" class="img-circle elevation-2" style="padding-left: 0.4rem !important;" alt="avatar">
+            <img src="img\profile-image.jpg" class="img-circle elevation-2" alt="avatar">
         @endif
     </div>
     <div class="info d-flex justify-content-center align-items-center">
