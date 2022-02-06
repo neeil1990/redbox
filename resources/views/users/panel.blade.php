bb6f583c04b8440479254d4209ac38d80d6880da
Nikolay <reddeveloper@gmail.com>
2022-02-06 18:27:35 +0300

    fix visual bugs

--- resources/views/users/panel.blade.php
+++ resources/views/users/panel.blade.php
@@ -3,7 +3,7 @@
         @if(isset($user->image))
             <img src="{{ Storage::url($user->image) }}" class="img-circle elevation-2" alt="{{ $user->name }}">
         @else
-            <img src="img\profile-image.jpg" class="img-circle elevation-2" alt="avatar">
+            <img src="img\profile-image.jpg" class="img-circle elevation-2" style="padding-left: 0.4rem !important;" alt="avatar">
         @endif
     </div>
     <div class="info d-flex justify-content-center align-items-center">
