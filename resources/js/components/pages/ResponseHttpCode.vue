ee55bdf8dff30615581bf97c469cea64956f198b
Nikolay <reddeveloper@gmail.com>
2022-02-07 18:53:23 +0300

    fix visual bugs

--- resources/js/components/pages/ResponseHttpCode.vue
+++ resources/js/components/pages/ResponseHttpCode.vue
@@ -25,39 +25,39 @@
         </div>
 
         <div class="row mb-4">
-            <div class="col-md-12 test-class">
+            <div class="col-md-6">
                 <div class="card" v-if="cardDisplay">
                     <div class="card-header">
                         <span v-for="(item, index) in codes" class="mr-2">
-                            <strong>{{ index }}</strong>:
-                            <span class="right badge badge-info">{{ item.length }}</span>
+                            <strong>{{index}}</strong>:
+                            <span class="right badge badge-info">{{item.length}}</span>
                         </span>
                     </div>
                     <div class="card-body">
                         <table class="table dataTable table-bordered">
                             <thead>
-                            <tr>
-                                <th style="width: 10px">#</th>
-                                <th>{{ urlTitle }}</th>
-                                <th class="sorting" @click.prevent="Sorting">{{ codeTitle }}</th>
-                                <th style="width: 40px"></th>
-                            </tr>
+                                <tr>
+                                    <th style="width: 10px">#</th>
+                                    <th>{{ urlTitle }}</th>
+                                    <th class="sorting" @click.prevent="Sorting">{{ codeTitle }}</th>
+                                    <th style="width: 40px"></th>
+                                </tr>
                             </thead>
 
                             <tbody>
-                            <tr v-for="item in items" :key="item.id">
-                                <td>{{ item.id + 1 }}</td>
-                                <td>{{ item.url }}</td>
-                                <td>{{ item.code }}</td>
-                                <td>
+                                <tr v-for="item in items" :key="item.id">
+                                    <td>{{ item.id + 1 }}</td>
+                                    <td>{{ item.url }}</td>
+                                    <td>{{ item.code }}</td>
+                                    <td>
                                     <span v-if="item.status" class="badge bg-success p-2">
                                         <i class="fas fa-check"></i>
                                     </span>
-                                    <span v-else class="badge bg-danger p-2">
+                                        <span v-else class="badge bg-danger p-2">
                                         <i class="far fa-times-circle"></i>
                                     </span>
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                         <a class="btn btn-info btn-sm" @click.prevent="ExportItems">
@@ -72,104 +72,104 @@
 </template>
 
 <script>
-export default {
-    name: "ResponseHttpCode",
-    props: {
-        submit: {
-            type: String
+    export default {
+        name: "ResponseHttpCode",
+        props:{
+            submit: {
+                type: String
+            },
+            urlTitle: {
+                type: String
+            },
+            codeTitle: {
+                type: String
+            },
+            textTitle: {
+                type: String
+            },
+            timeoutTitle: {
+                type: String
+            },
+            exportBtn: {
+                type: String
+            }
         },
-        urlTitle: {
-            type: String
+        data(){
+          return {
+              time: 1000,
+              order: 'desc',
+              urls: '',
+              arUrls: [],
+              items: [],
+              codes: {},
+          }
         },
-        codeTitle: {
-            type: String
+        computed: {
+
+            cardDisplay: function() {
+               return this.items.length > 0;
+            }
         },
-        textTitle: {
-            type: String
-        },
-        timeoutTitle: {
-            type: String
-        },
-        exportBtn: {
-            type: String
-        }
-    },
-    data() {
-        return {
-            time: 1000,
-            order: 'desc',
-            urls: '',
-            arUrls: [],
-            items: [],
-            codes: {},
-        }
-    },
-    computed: {
+        methods: {
+            ExportItems() {
+                let str = JSON.stringify(this.items);
+                let buf = new Buffer(str);
+                let base64data = buf.toString('base64');
 
-        cardDisplay: function () {
-            return this.items.length > 0;
-        }
-    },
-    methods: {
-        ExportItems() {
-            let str = JSON.stringify(this.items);
-            let buf = new Buffer(str);
-            let base64data = buf.toString('base64');
+                window.location.href = "http-headers/"+ base64data +"/export/";
+            },
+            ShowHttpResponse() {
+                var app = this;
 
-            window.location.href = "http-headers/" + base64data + "/export/";
-        },
-        ShowHttpResponse() {
-            var app = this;
+                app.items = [];
+                app.codes = {};
 
-            app.items = [];
-            app.codes = {};
+                app.StringToArray();
 
-            app.StringToArray();
+                app.arUrls.forEach((element,i) => {
+                    if(i >= 500)
+                        return false;
 
-            app.arUrls.forEach((element, i) => {
-                if (i >= 500)
-                    return false;
+                    setTimeout(() => {
+                        app.HttpRequest(element, i);
+                    }, i * app.time);
+                });
+            },
 
-                setTimeout(() => {
-                    app.HttpRequest(element, i);
-                }, i * app.time);
-            });
-        },
+            Sorting() {
+                if(this.order === 'desc')
+                    this.order = 'asc';
+                else
+                    this.order = 'desc';
 
-        Sorting() {
-            if (this.order === 'desc')
-                this.order = 'asc';
-            else
-                this.order = 'desc';
+                this.items = _.orderBy(this.items, 'code', this.order);
+            },
 
-            this.items = _.orderBy(this.items, 'code', this.order);
-        },
+            HttpRequest(url, i) {
+                var app = this;
 
-        HttpRequest(url, i) {
-            var app = this;
+                axios.get('/http-headers', {
+                    params: {
+                        url: url,
+                        http: 1
+                    }
+                }).then(function (response) {
+                    let code = response.data;
+                    let status = response.data === 200;
 
-            axios.get('/http-headers', {
-                params: {
-                    url: url,
-                    http: 1
-                }
-            }).then(function (response) {
-                let code = response.data;
-                let status = response.data === 200;
+                    if(app.codes[response.data] === undefined)
+                        app.codes[response.data] = [];
 
-                if (app.codes[response.data] === undefined)
-                    app.codes[response.data] = [];
+                    app.codes[response.data].push(response.data);
 
-                app.codes[response.data].push(response.data);
+                    app.items.push({id: i, url: url, code: code, status: status});
+                });
+            },
 
-                app.items.push({id: i, url: url, code: code, status: status});
-            });
-        },
-
-        StringToArray() {
-            if (this.urls.length)
-                this.arUrls = _.compact(this.urls.split(/[\r\n]+/));
+            StringToArray() {
+                if(this.urls.length)
+                    this.arUrls = _.compact(this.urls.split(/[\r\n]+/));
+            }
         }
     }
-}
 </script>
