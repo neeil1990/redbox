3e6b2ee8db98a97503e84297dacd4f2df4869195
Nikolay <reddeveloper@gmail.com>
2022-02-26 03:39:19 +0300

    fix

--- app/Http/Controllers/CroneController.php
+++ app/Http/Controllers/CroneController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\DomainMonitoring;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use PHPUnit\Exception;
 
@@ -14,18 +15,19 @@ class CroneController extends Controller
      */
     public function checkLinkCrone($timing)
     {
+        Log::debug('start scan', [Carbon::now()]);
         try {
-            if (!file_exists($timing . '.txt')) {
-                file_put_contents($timing . '.txt', '', 8);
-                $projects = DomainMonitoring::where('timing', '=', $timing)->get();
-                foreach ($projects as $project) {
-                    DomainMonitoring::httpCheck($project);
-                }
-                unlink($timing . '.txt');
+//            if (!file_exists($timing . '.txt')) {
+//                file_put_contents($timing . '.txt', '', 8);
+            $projects = DomainMonitoring::where('timing', '=', $timing)->get();
+            foreach ($projects as $project) {
+                DomainMonitoring::httpCheck($project);
             }
+            unlink($timing . '.txt');
+//            }
         } catch (Exception $exception) {
             Log::debug('scan error', [$exception->getMessage()]);
-            unlink($timing . '.txt');
+//            unlink($timing . '.txt');
         }
     }
 }
--- app/Http/Controllers/DomainMonitoringController.php
+++ app/Http/Controllers/DomainMonitoringController.php
@@ -9,9 +9,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Redirect;
-use Symfony\Component\VarDumper\VarDumper;
 
 class DomainMonitoringController extends Controller
 {
