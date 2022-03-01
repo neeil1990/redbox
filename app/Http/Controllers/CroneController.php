859264581a7a123d81716f1f5c7528510bec8c21
Nikolay <reddeveloper@gmail.com>
2022-03-01 18:17:45 +0300

    fix bug

--- app/Http/Controllers/CroneController.php
+++ app/Http/Controllers/CroneController.php
@@ -16,17 +16,12 @@ class CroneController extends Controller
     {
         Log::debug('start monitoring with timing', [$timing]);
         try {
-            if (!file_exists($timing . '.txt')) {
-                file_put_contents($timing . '.txt', '', 8);
-                $projects = DomainMonitoring::where('timing', '=', $timing)->get();
-                foreach ($projects as $project) {
-                    DomainMonitoring::httpCheck($project);
-                }
-                unlink($timing . '.txt');
+            $projects = DomainMonitoring::where('timing', '=', $timing)->get();
+            foreach ($projects as $project) {
+                DomainMonitoring::httpCheck($project);
             }
         } catch (Exception $exception) {
             Log::debug('scan error', [$exception->getMessage()]);
-            unlink($timing . '.txt');
         }
     }
 }
