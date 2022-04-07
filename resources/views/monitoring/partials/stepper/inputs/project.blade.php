cddfb41838e73ef777e962ea1d320847c1c6a9da
neeil1990 <neeil@mail.ru>
2022-04-07 12:55:14 +0300

    #1981460 validation

--- resources/views/monitoring/partials/stepper/inputs/project.blade.php
+++ resources/views/monitoring/partials/stepper/inputs/project.blade.php
@@ -1,11 +1,11 @@
 <div class="form-group">
     {!! Form::label('name', 'Имя проекта') !!}
-    {!! Form::text('name', null, ['class' => 'form-control' . ($errors->has('name') ? ' is-invalid' : ''), 'placeholder' => 'Введите имя проекта']) !!}
-    @error('name') <span class="error invalid-feedback d-block">{{ $message }}</span> @enderror
+    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Введите имя проекта', 'required']) !!}
+    <div class="invalid-feedback">{{ __('Please fill the field') }}</div>
 </div>
 
 <div class="form-group">
     {!! Form::label('url', 'URL домена') !!}
-    {!! Form::text('url', null, ['class' => 'form-control' . ($errors->has('url') ? ' is-invalid' : ''), 'placeholder' => 'Введите URL домена (example.com) или страницы (example.com/page/)']) !!}
-    @error('url') <span class="error invalid-feedback d-block">{{ $message }}</span> @enderror
+    {!! Form::text('url', null, ['class' => 'form-control', 'placeholder' => 'Введите URL домена (example.com) или страницы (example.com/page/)', 'required']) !!}
+    <div class="invalid-feedback">{{ __('Please fill the field') }}</div>
 </div>
---
+++ resources/views/monitoring/partials/stepper/inputs/regions/yandex.blade.php
@@ -0,0 +1,13 @@
+<div class="form-group">
+    <label>Выберете регионы для поисковой системы Яндекс.</label>
+    <select name="regions[yandex][]" class="select2bs4" multiple="multiple" style="width: 100%;">
+        <option>Alabama</option>
+        <option>Alaska</option>
+        <option>California</option>
+        <option>Delaware</option>
+        <option>Tennessee</option>
+        <option>Texas</option>
+        <option>Washington</option>
+    </select>
+</div>
+<!-- /.form-group -->
