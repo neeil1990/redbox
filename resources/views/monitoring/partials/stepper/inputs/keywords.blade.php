ce52732a823479d43714a45064a11adf889c905a
neeil1990 <neeil@mail.ru>
2022-05-11 10:55:16 +0300

    #138298 target

--- resources/views/monitoring/partials/stepper/inputs/keywords.blade.php
+++ resources/views/monitoring/partials/stepper/inputs/keywords.blade.php
@@ -103,7 +103,7 @@
                                 <option value="1">1</option>
                                 <option value="3">3</option>
                                 <option value="5">5</option>
-                                <option value="10">10</option>
+                                <option value="10" selected>10</option>
                                 <option value="20">20</option>
                                 <option value="50">50</option>
                                 <option value="100">100</option>
