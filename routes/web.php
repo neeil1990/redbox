c474f6a4b33aa1328be8ab37f707448dfb68692f
Nikolay <reddeveloper@gmail.com>
2022-06-02 00:22:36 +0300

    #1357 history state changes

--- routes/web.php
+++ routes/web.php
@@ -208,3 +208,8 @@ Route::middleware(['verified'])->group(function () {
     Route::resource('monitoring/groups', 'MonitoringGroupsController');
     Route::post('monitoring/keywords/queue', 'MonitoringKeywordsController@addingQueue')->name('keywords.queue');
 });
+
+
+Route::get('/ttest', function () {
+    dump(config('database'));
+});
