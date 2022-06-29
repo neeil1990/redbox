0615b44aa107ba2402e59da00a10a3839482b595
Nikolay <reddeveloper@gmail.com>
2022-06-29 16:03:33 +0300

    cleanup telgram bot code

--- app/TelegramBot.php
+++ app/TelegramBot.php
@@ -226,7 +226,7 @@ class TelegramBot extends Model
      */
     public static function sendMessage($text, $chatId)
     {
-        if ($chatId) {
+        try {
             $data = [
                 'text' => $text,
                 'chat_id' => $chatId,
@@ -237,7 +237,10 @@ class TelegramBot extends Model
             file_get_contents("https://api.telegram.org/bot2073017935:AAF5OJbt74xrX8W7kR_O4NhSMWncpTiwflo/sendMessage?chat_id=$chatId"
                 . http_build_query($data)
             );
+        } catch (\Throwable $exception) {
+
         }
+
 
         return http_response_code(200);
     }
