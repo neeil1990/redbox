7919a85f986ee6ed020294dbcf2c7ef0b337814c
neeil1990 <neeil@mail.ru>
2022-06-30 10:54:27 +0300

    #138298 serach table

---
+++ resources/views/monitoring/partials/show/header/group.blade.php
@@ -0,0 +1,5 @@
+<span style="white-space: nowrap;">
+    <b>{{ __('Group') }}</b>
+    <input type="text" class="form-control form-control-sm form-control-border d-none" placeholder="Search: {{ __('Group') }}" style="min-width: 200px">
+    <a href="javascript:void(0)" class="search-button"><i class="fas fa-search text-muted"></i></a>
+</span>
