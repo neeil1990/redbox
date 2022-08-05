105fb100e10d94160b9c0a653cf5bb6676d2ef3f
Nikolay <tifilum74@mail.ru>
2022-08-05 11:02:48 +0300

    added though scan in all relevance project page

--- public/plugins/relevance-analysis/history/mainHistoryTable.js
+++ public/plugins/relevance-analysis/history/mainHistoryTable.js
@@ -45,72 +45,74 @@ $('.start-through-analyse').on('click', function () {
     $('.though-render').remove()
     let thoughTable = $('#though-table')
     thoughTable.dataTable().fnDestroy();
-    $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "/start-through-analyse",
-        data: {
-            _token: $('meta[name="csrf-token"]').attr('content'),
-            id: $(this).attr('data-target'),
-        },
-        success: function (response) {
-            if (response.code === 200) {
-                getSuccessMessage(response.message, 5000)
-                $.each(JSON.parse(response.object), function (key, value) {
-                    let thoughLinks = ''
-                    $.each(value['throughLinks'], function (tkey, tvalue) {
-                        let url = new URL(tkey)
-                        thoughLinks +=
-                            '<tr>' +
-                            '   <td>' + tvalue + '</td>' +
-                            '   <td><a href="' + tkey + '" target="_blank" title="' + tkey + '"> ' + url.origin + ' </a></td>' +
+    setTimeout(() => {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/start-through-analyse",
+            data: {
+                _token: $('meta[name="csrf-token"]').attr('content'),
+                id: $(this).attr('data-target'),
+            },
+            success: function (response) {
+                if (response.code === 200) {
+                    getSuccessMessage(response.message, 5000)
+                    $.each(JSON.parse(response.object), function (key, value) {
+                        let thoughLinks = ''
+                        $.each(value['throughLinks'], function (tkey, tvalue) {
+                            let url = new URL(tkey)
+                            thoughLinks +=
+                                '<tr>' +
+                                '   <td><a href="' + tkey + '" target="_blank" title="' + tkey + '"> ' + url.origin + ' </a></td>' +
+                                '   <td>' + tvalue + '</td>' +
+                                '</tr>'
+                        })
+                        let newTable =
+                            '<table>' +
+                            '   <thead>' +
+                            '       <tr>' +
+                            '           <td>Ссылка на сайт</td>' +
+                            '           <td>Количество повторений</td>' +
+                            '       </tr>' +
+                            '   </thead>' +
+                            '   <tbody>' +
+                            thoughLinks +
+                            '   </tbody>' +
+                            '</table>'
+
+                        $('#though-table-body').append(
+                            '<tr class="though-render">' +
+                            '   <td class="col-2">' + key + '</td>' +
+                            '   <td class="col-2">' + newTable + '</td>' +
+                            '   <td class="col-2">' + value['tf'] + '</td>' +
+                            '   <td class="col-2">' + value['idf'] + '</td>' +
+                            '   <td class="col-1">' + value['repeatInTextMainPage'] + '</td>' +
+                            '   <td class="col-1">' + value['repeatInLinkMainPage'] + '</td>' +
+                            '   <td class="col-2" data-target="' + value['throughCount'] + '">' + value['throughCount'] + '/' + value['total'] + '</td>' +
                             '</tr>'
-                    })
-                    let newTable =
-                        '<table>' +
-                        '   <thead>' +
-                        '       <tr>' +
-                        '           <td>Количество повторений</td>' +
-                        '           <td>Ссылка на сайт</td>' +
-                        '       </tr>' +
-                        '   </thead>' +
-                        '   <tbody>' +
-                        thoughLinks +
-                        '   </tbody>' +
-                        '</table>'
+                        )
+                    });
 
-                    $('#though-table-body').append(
-                        '<tr class="though-render">' +
-                        '   <td class="col-2">' + key + '</td>' +
-                        '   <td class="col-2">' + newTable + '</td>' +
-                        '   <td class="col-2">' + value['tf'] + '</td>' +
-                        '   <td class="col-2">' + value['idf'] + '</td>' +
-                        '   <td class="col-1">' + value['repeatInTextMainPage'] + '</td>' +
-                        '   <td class="col-1">' + value['repeatInLinkMainPage'] + '</td>' +
-                        '   <td class="col-2" data-target="' + value['throughCount'] + '">' + value['throughCount'] + '/' + value['total'] + '</td>' +
-                        '</tr>'
-                    )
-                });
+                    thoughTable.DataTable({
+                        "order": [[2, "desc"]],
+                        "pageLength": 50,
+                        "searching": true,
+                        dom: 'lBfrtip',
+                        buttons: [
+                            'copy', 'csv', 'excel'
+                        ]
+                    });
 
-                thoughTable.DataTable({
-                    "order": [[2, "desc"]],
-                    "pageLength": 50,
-                    "searching": true,
-                    dom: 'lBfrtip',
-                    buttons: [
-                        'copy', 'csv', 'excel'
-                    ]
-                });
+                    $(".dt-button").addClass('btn btn-secondary')
 
-                $(".dt-button").addClass('btn btn-secondary')
-
-                setTimeout(() => {
-                    $('#though-block').show()
-                    scrollTo('#though-block')
-                }, 500)
-            } else if (response.code === 415) {
-                getErrorMessage(response.message, 15000)
-            }
-        },
-    });
+                    setTimeout(() => {
+                        $('#though-block').show()
+                        scrollTo('#though-block')
+                    }, 500)
+                } else if (response.code === 415) {
+                    getErrorMessage(response.message, 15000)
+                }
+            },
+        });
+    }, 500)
 })
