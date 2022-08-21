326507f2fa8d4e39dce397111ceaa1ad2d65fa18
Nikolay <tifilum74@mail.ru>
2022-08-21 16:22:45 +0300

    added new version for scan root words

--- app/Morphy.php
+++ app/Morphy.php
@@ -5,7 +5,7 @@ namespace App;
 use phpMorphy;
 use phpMorphy_FilesBundle;
 
-require_once __DIR__ . '/../public/plugins/phpmorphy/src/common.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../public/plugins/phpmorphy/src/common.php';
 
 class Morphy
 {
