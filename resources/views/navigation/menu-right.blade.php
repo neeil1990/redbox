89da4bc28c0722b027496a5a33208079411f1281
Nikolay <tifilum74@mail.ru>
2022-09-05 14:37:41 +0300

    fix visual bug

--- resources/views/navigation/menu-right.blade.php
+++ resources/views/navigation/menu-right.blade.php
@@ -12,45 +12,44 @@
     </li>
 @endif
 
-<div class="dropdown p-0 m-0 nav-item">
-    @if($tariffs != [])
+@if($tariffs != [])
+    <div class="dropdown p-0 m-0 nav-item">
         <span class="dropdown-toggle nav-link" role="button" data-toggle="dropdown" aria-expanded="false">
-        Ваши лимиты
-    </span>
-    @endif
+            Ваши лимиты
+        </span>
+        <div class="dropdown-menu p-0 m-0" style="width: 410px">
+            <table class="table table-bordered p-0 m-0">
+                <thead>
+                <tr>
+                    <th>Модуль</th>
+                    <th>Лимиты</th>
+                    <th>Исчерпано</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($tariffs as $key => $tariff)
+                    @if($key != 'price')
+                        <tr class="{{ $key }}">
+                            <td>{{ $tariff['name'] }}</td>
+                            <td>{{ $tariff['value'] }}</td>
+                            <td>
+                                <div class="progress progress-xs">
+                                    <div class="progress-bar progress-bar-danger"
+                                         style="width: {{ $tariff['percent'] }}%"></div>
+                                </div>
+                                @if(gettype($tariff['used']) == 'integer')
+                                    Осталось {{ $tariff['value'] - $tariff['used'] }}
+                                @else
+                                    {{ $tariff['used'] }}
+                                @endif
 
-    <div class="dropdown-menu p-0 m-0" style="width: 410px">
-        <table class="table table-bordered p-0 m-0">
-            <thead>
-            <tr>
-                <th>Модуль</th>
-                <th>Лимиты</th>
-                <th>Исчерпано</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($tariffs as $key => $tariff)
-                @if($key != 'price')
-                    <tr class="{{ $key }}">
-                        <td>{{ $tariff['name'] }}</td>
-                        <td>{{ $tariff['value'] }}</td>
-                        <td>
-                            <div class="progress progress-xs">
-                                <div class="progress-bar progress-bar-danger"
-                                     style="width: {{ $tariff['percent'] }}%"></div>
-                            </div>
-                            @if(gettype($tariff['used']) == 'integer')
-                                Осталось {{ $tariff['value'] - $tariff['used'] }}
-                            @else
-                                {{ $tariff['used'] }}
-                            @endif
-
-                        </td>
-                    </tr>
-                @endif
-            @endforeach
-            </tbody>
-        </table>
+                            </td>
+                        </tr>
+                    @endif
+                @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-</div>
+@endif
 
