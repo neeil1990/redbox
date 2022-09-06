91b1c500117c6c3c26c3dd1b1b1a4569cc75e302
Nikolay <tifilum74@mail.ru>
2022-09-06 19:03:45 +0300

    unreturned menu sort

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,7 +92,7 @@
 
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+{{--<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>--}}
 
 <script>
     $(function () {
