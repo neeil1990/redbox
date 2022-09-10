cc6f896b27607c4f8c3b9d20910c5e31070a41a4
Nikolay <tifilum74@mail.ru>
2022-09-10 18:00:52 +0300

    added info limits

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('img/favicon.svg') }}"/>
     <title>@yield('title')</title>
     <!-- Google Font: Source Sans Pro -->
