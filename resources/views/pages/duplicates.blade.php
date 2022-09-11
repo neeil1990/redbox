99d3335dd6e7a405713eb13217feab0d811ec79c
Nikolay <tifilum74@mail.ru>
2022-09-11 05:08:25 +0300

    changed ru.json

--- resources/views/pages/duplicates.blade.php
+++ resources/views/pages/duplicates.blade.php
@@ -1,5 +1,11 @@
 @component('component.card', ['title' => __('Remove Duplicates')])
 
+    <style>
+        .RemoveDublicate {
+            background: oldlace;
+        }
+    </style>
+
     <remove-duplicates :names="{{ $options }}"
                        start="{{ __('remove characters at the beginning of a word') }}: +-!"
                        end="{{ __('remove characters at the end of a word') }}: .!?"
