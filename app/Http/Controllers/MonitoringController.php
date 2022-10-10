1ea1a3000e72aaaace3cd5f4538cfd93be014d0d
neeil1990 <neeil@mail.ru>
2022-10-10 12:46:00 +0300

    #1981460 frontend

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -694,12 +694,12 @@ class MonitoringController extends Controller
         $count = $user->monitoringProjects()->count();
 
         $navigations = [
-            ['h3' => $count, 'p' => 'Проекты', 'icon' => 'fas fa-bezier-curve', 'href' => route('monitoring.index'), 'bg' => 'bg-info'],
-            ['h3' => '150', 'p' => 'Мои конкуренты', 'icon' => 'fas fa-user-secret', 'href' => '#', 'bg' => 'bg-success'],
-            ['h3' => '150', 'p' => 'Анализ ТОП-100', 'icon' => 'fas fa-chart-pie', 'href' => '#', 'bg' => 'bg-warning'],
-            ['h3' => '150', 'p' => 'План продвижения', 'icon' => 'far fa-check-square', 'href' => '#', 'bg' => 'bg-danger'],
-            ['h3' => '150', 'p' => 'Аудит сайта', 'icon' => 'fas fa-tasks', 'href' => '#', 'bg' => 'bg-info'],
-            ['h3' => '150', 'p' => 'Отслеживание ссылок', 'icon' => 'fas fa-link', 'href' => route('backlink'), 'bg' => 'bg-purple-light'],
+            ['h3' => $count, 'p' => 'Проекты', 'small' => '', 'icon' => 'fas fa-bezier-curve', 'href' => route('monitoring.index'), 'bg' => 'bg-info'],
+            ['h3' => '150', 'p' => 'Мои конкуренты', 'small' => 'В разработке', 'icon' => 'fas fa-user-secret', 'href' => '#', 'bg' => 'bg-success'],
+            ['h3' => '150', 'p' => 'Анализ ТОП-100', 'small' => 'В разработке', 'icon' => 'fas fa-chart-pie', 'href' => '#', 'bg' => 'bg-warning'],
+            ['h3' => '150', 'p' => 'План продвижения', 'small' => 'В разработке', 'icon' => 'far fa-check-square', 'href' => '#', 'bg' => 'bg-danger'],
+            ['h3' => '150', 'p' => 'Аудит сайта', 'small' => 'В разработке', 'icon' => 'fas fa-tasks', 'href' => '#', 'bg' => 'bg-info'],
+            ['h3' => '150', 'p' => 'Отслеживание ссылок', 'small' => '', 'icon' => 'fas fa-link', 'href' => route('backlink'), 'bg' => 'bg-purple-light'],
         ];
 
         return $navigations;
--- resources/views/monitoring/keywords/controls.blade.php
+++ resources/views/monitoring/keywords/controls.blade.php
@@ -1,29 +1,33 @@
 
 <!-- Check all button -->
-<button type="button" class="btn btn-default btn-sm checkbox-toggle">
+<button type="button" class="btn btn-default btn-sm checkbox-toggle tooltip-on" title="Выбрать все">
     <i class="far fa-square"></i>
 </button>
+
 <div class="btn-group">
-    <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target=".modal" data-type="create_keywords">
+    <button type="button" class="btn btn-default btn-sm tooltip-on" data-toggle="modal" data-target=".modal" data-type="create_keywords" title="Добавить запрос">
         <i class="fas fa-plus"></i>
     </button>
 
-    <button type="button" class="btn btn-default btn-sm" data-toggle="modal" data-target=".modal" data-type="edit_plural">
+    <button type="button" class="btn btn-default btn-sm tooltip-on" data-toggle="modal" data-target=".modal" data-type="edit_plural" title="Редактировать запросы">
         <i class="fas fa-pen"></i>
     </button>
 
-    <button type="button" class="btn btn-default btn-sm delete-multiple">
+    <button type="button" class="btn btn-default btn-sm delete-multiple tooltip-on" title="Удалить запросы">
         <i class="far fa-trash-alt"></i>
     </button>
 </div>
 <!-- /.btn-group -->
-<button type="button" class="btn btn-default btn-sm parse-positions">
-    <i class="fas fa-sync-alt"></i>
-</button>
 
-<button type="button" class="btn btn-default btn-sm parse-positions-keys">
-    <i class="fas fa-key"></i>
-</button>
+<div class="btn-group">
+    <button type="button" class="btn btn-default btn-sm parse-positions-keys tooltip-on" title="Добавить в очередь выбранные">
+        <i class="fas fa-layer-group"></i>
+    </button>
+
+    <button type="button" class="btn btn-default btn-sm parse-positions tooltip-on" title="Добавить в очередь все">
+        <i class="fas fa-sync-alt"></i>
+    </button>
+</div>
 
 <div class="float-right"></div>
 <!-- /.float-right -->
