b692e8a678665f9048a5107edcfc4987b4d5ec13
Nikolay <tifilum74@mail.ru>
2022-12-09 00:38:30 +0300

    hot fix

--- public/plugins/cluster/js/render-result-fast-table.js
+++ public/plugins/cluster/js/render-result-fast-table.js
@@ -44,14 +44,12 @@ function renderResultTableFast(data, count, target) {
 
                 let minimum = ''
                 if ('minimum' in information) {
-                    $.each(information['minimum'], function (key, value) {
-                        minimum = '<span class="__helper-link ui_tooltip_w" style="background: brown">' +
-                            '      <i class="fa fa-question"></i>' +
-                            '      <span class="ui_tooltip __right" style="min-width: 550px;">' +
-                            '          <span class="ui_tooltip_content">' + key + '<br> ' + value + '</span>' +
-                            '      </span>' +
-                            '  </span>'
-                    })
+                    minimum = '<span class="__helper-link ui_tooltip_w" style="background: brown">' +
+                        '      <i class="fa fa-question"></i>' +
+                        '      <span class="ui_tooltip __right" style="min-width: 550px;">' +
+                        '          <span class="ui_tooltip_content">' + information['minimum'] + '</span>' +
+                        '      </span>' +
+                        '  </span>'
                 }
 
                 newTableRows +=
