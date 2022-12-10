139f0c54c5b5f6df750838e1232b6290775c403f
Nikolay <tifilum74@mail.ru>
2022-12-10 16:28:05 +0300

    metrics

--- resources/views/balance/metrics.blade.php
+++ resources/views/balance/metrics.blade.php
@@ -14,7 +14,7 @@
             k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
         (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(89500732, "init", {
+        ym(274563941, "init", {
             clickmap:true,
             trackLinks:true,
             accurateTrackBounce:true,
