c1096667837faaa5f0bf900469dd1649b3deb01d
Nikolay <tifilum74@mail.ru>
2022-12-16 17:28:02 +0300

    minify css/js

--- resources/views/cluster/index.blade.php
+++ resources/views/cluster/index.blade.php
@@ -148,9 +148,8 @@
             }
         </script>
         <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-        {{--        <script src="{{ asset('/plugins/cluster/js/render-hidden-table.js') }}"></script>--}}
-        <script src="{{ asset('/plugins/cluster/js/render-result-table.js') }}"></script>
-        <script src="{{ asset('/plugins/cluster/js/common.js') }}"></script>
+        <script src="{{ asset('/plugins/cluster/js/render-result-table.min.js') }}"></script>
+        <script src="{{ asset('/plugins/cluster/js/common.min.js') }}"></script>
         <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
