188e5278a8c5b142282ec66d343b54514037012b
Nikolay <tifilum74@mail.ru>
2022-12-17 12:57:11 +0300

    hot fix limits

--- resources/views/pages/duplicates.blade.php
+++ resources/views/pages/duplicates.blade.php
@@ -1,17 +1,13 @@
 @component('component.card', ['title' => __('Remove Duplicates')])
-
-    @slot('css')
-        <style>
-            .Duplicates {
-                background: oldlace;
-            }
-        </style>
-    @endslot
-
     <remove-duplicates :names="{{ $options }}"
                        start="{{ __('remove characters at the beginning of a word') }}: +-!"
                        end="{{ __('remove characters at the end of a word') }}: .!?"
                        submit="{{ __('Remove duplicates') }}"
     ></remove-duplicates>
 
+    <script>
+        $('.Duplicates').css({
+            'background': 'oldlace'
+        })
+    </script>
 @endcomponent
