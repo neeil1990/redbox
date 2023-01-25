6f1a73e2eab3296084ba81e7018a5985a941711f
Nikolay <tifilum74@mail.ru>
2023-01-25 23:42:07 +0300

    hor fix

--- resources/views/relevance-analysis/all.blade.php
+++ resources/views/relevance-analysis/all.blade.php
@@ -886,153 +886,153 @@
         <script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
         <script src="https://cdn.datatables.net/plug-ins/1.12.0/sorting/date-dd-MMM-yyyy.js"></script>
         <script>
-            $('input#switchMyListWords').click(function () {
-                if ($(this).is(':checked')) {
-                    $('.form-group.required.list-words.mt-1').show(300)
-                } else {
-                    $('.form-group.required.list-words.mt-1').hide(300)
-                }
-            })
-
-            function getRegionName(id) {
-                switch (id) {
-                    case '1' :
-                        return "{{ __('Moscow') }}";
-                    case '20' :
-                        return "{{ __('Arkhangelsk') }}";
-                    case '37' :
-                        return "{{ __('Astrakhan') }}";
-                    case '197' :
-                        return "{{ __('Barnaul') }}";
-                    case '4' :
-                        return "{{ __('Belgorod') }}";
-                    case '77' :
-                        return "{{ __('Blagoveshchensk') }}";
-                    case '191' :
-                        return "{{ __('Bryansk') }}";
-                    case '24' :
-                        return "{{ __('Veliky Novgorod') }}";
-                    case '75' :
-                        return "{{ __('Vladivostok') }}";
-                    case '33' :
-                        return "{{ __('Vladikavkaz') }}";
-                    case '192' :
-                        return "{{ __('Vladimir') }}";
-                    case '38' :
-                        return "{{ __('Volgograd') }}";
-                    case '21' :
-                        return "{{ __('Vologda') }}";
-                    case '193' :
-                        return "{{ __('Voronezh') }}";
-                    case '1106' :
-                        return "{{ __('Grozny') }}";
-                    case '54' :
-                        return "{{ __('Ekaterinburg') }}";
-                    case '5' :
-                        return "{{ __('Ivanovo') }}";
-                    case '63' :
-                        return "{{ __('Irkutsk') }}";
-                    case '41' :
-                        return "{{ __('Yoshkar-ola') }}";
-                    case '43' :
-                        return "{{ __('Kazan') }}";
-                    case '22' :
-                        return "{{ __('Kaliningrad') }}";
-                    case '64' :
-                        return "{{ __('Kemerovo') }}";
-                    case '7' :
-                        return "{{ __('Kostroma') }}";
-                    case '35' :
-                        return "{{ __('Krasnodar') }}";
-                    case '62' :
-                        return "{{ __('Krasnoyarsk') }}";
-                    case '53' :
-                        return "{{ __('Kurgan') }}";
-                    case '8' :
-                        return "{{ __('Kursk') }}";
-                    case '9' :
-                        return "{{ __('Lipetsk') }}";
-                    case '28' :
-                        return "{{ __('Makhachkala') }}";
-                    case '213' :
-                        return "{{ __('Moscow') }}";
-                    case '23' :
-                        return "{{ __('Murmansk') }}";
-                    case '1092' :
-                        return "{{ __('Nazran') }}";
-                    case '30' :
-                        return "{{ __('Nalchik') }}";
-                    case '47' :
-                        return "{{ __('Nizhniy Novgorod') }}";
-                    case '65' :
-                        return "{{ __('Novosibirsk') }}";
-                    case '66' :
-                        return "{{ __('Omsk') }}";
-                    case '10' :
-                        return "{{ __('Eagle') }}";
-                    case '48' :
-                        return "{{ __('Orenburg') }}";
-                    case '49' :
-                        return "{{ __('Penza') }}";
-                    case '50' :
-                        return "{{ __('Perm') }}";
-                    case '25' :
-                        return "{{ __('Pskov') }}";
-                    case '39' :
-                        return "{{ __('Rostov-on') }}";
-                    case '11' :
-                        return "{{ __('Ryazan') }}";
-                    case '51' :
-                        return "{{ __('Samara') }}";
-                    case '42' :
-                        return "{{ __('Saransk') }}";
-                    case '2' :
-                        return "{{ __('Saint-Petersburg') }}";
-                    case '12' :
-                        return "{{ __('Smolensk') }}";
-                    case '239' :
-                        return "{{ __('Sochi') }}";
-                    case '36' :
-                        return "{{ __('Stavropol') }}";
-                    case '973' :
-                        return "{{ __('Surgut') }}";
-                    case '13' :
-                        return "{{ __('Tambov') }}";
-                    case '14' :
-                        return "{{ __('Tver') }}";
-                    case '67' :
-                        return "{{ __('Tomsk') }}";
-                    case '15' :
-                        return "{{ __('Tula') }}";
-                    case '195' :
-                        return "{{ __('Ulyanovsk') }}";
-                    case '172' :
-                        return "{{ __('Ufa') }}";
-                    case '76' :
-                        return "{{ __('Khabarovsk') }}";
-                    case '45' :
-                        return "{{ __('Cheboksary') }}";
-                    case '56' :
-                        return "{{ __('Chelyabinsk') }}";
-                    case '1104' :
-                        return "{{ __('Cherkessk') }}";
-                    case '16' :
-                        return "{{ __('Yaroslavl') }}";
-                }
-            }
+            let words = {
+                search: "{{ __('Search') }}",
+                show: "{{ __('show') }}",
+                records: "{{ __('records') }}",
+                noRecords: "{{ __('No records') }}",
+                showing: "{{ __('Showing') }}",
+                from: "{{ __('from') }}",
+                to: "{{ __('to') }}",
+                of: "{{ __('of') }}",
+                entries: "{{ __('entries') }}"
+            };
 
             $(document).ready(function () {
-                let words = {
-                    search: "{{ __('Search') }}",
-                    show: "{{ __('show') }}",
-                    records: "{{ __('records') }}",
-                    noRecords: "{{ __('No records') }}",
-                    showing: "{{ __('Showing') }}",
-                    from: "{{ __('from') }}",
-                    to: "{{ __('to') }}",
-                    of: "{{ __('of') }}",
-                    entries: "{{ __('entries') }}"
-                };
+                $('input#switchMyListWords').click(function () {
+                    if ($(this).is(':checked')) {
+                        $('.form-group.required.list-words.mt-1').show(300)
+                    } else {
+                        $('.form-group.required.list-words.mt-1').hide(300)
+                    }
+                })
+
+                function getRegionName(id) {
+                    switch (id) {
+                        case '1' :
+                            return "{{ __('Moscow') }}";
+                        case '20' :
+                            return "{{ __('Arkhangelsk') }}";
+                        case '37' :
+                            return "{{ __('Astrakhan') }}";
+                        case '197' :
+                            return "{{ __('Barnaul') }}";
+                        case '4' :
+                            return "{{ __('Belgorod') }}";
+                        case '77' :
+                            return "{{ __('Blagoveshchensk') }}";
+                        case '191' :
+                            return "{{ __('Bryansk') }}";
+                        case '24' :
+                            return "{{ __('Veliky Novgorod') }}";
+                        case '75' :
+                            return "{{ __('Vladivostok') }}";
+                        case '33' :
+                            return "{{ __('Vladikavkaz') }}";
+                        case '192' :
+                            return "{{ __('Vladimir') }}";
+                        case '38' :
+                            return "{{ __('Volgograd') }}";
+                        case '21' :
+                            return "{{ __('Vologda') }}";
+                        case '193' :
+                            return "{{ __('Voronezh') }}";
+                        case '1106' :
+                            return "{{ __('Grozny') }}";
+                        case '54' :
+                            return "{{ __('Ekaterinburg') }}";
+                        case '5' :
+                            return "{{ __('Ivanovo') }}";
+                        case '63' :
+                            return "{{ __('Irkutsk') }}";
+                        case '41' :
+                            return "{{ __('Yoshkar-ola') }}";
+                        case '43' :
+                            return "{{ __('Kazan') }}";
+                        case '22' :
+                            return "{{ __('Kaliningrad') }}";
+                        case '64' :
+                            return "{{ __('Kemerovo') }}";
+                        case '7' :
+                            return "{{ __('Kostroma') }}";
+                        case '35' :
+                            return "{{ __('Krasnodar') }}";
+                        case '62' :
+                            return "{{ __('Krasnoyarsk') }}";
+                        case '53' :
+                            return "{{ __('Kurgan') }}";
+                        case '8' :
+                            return "{{ __('Kursk') }}";
+                        case '9' :
+                            return "{{ __('Lipetsk') }}";
+                        case '28' :
+                            return "{{ __('Makhachkala') }}";
+                        case '213' :
+                            return "{{ __('Moscow') }}";
+                        case '23' :
+                            return "{{ __('Murmansk') }}";
+                        case '1092' :
+                            return "{{ __('Nazran') }}";
+                        case '30' :
+                            return "{{ __('Nalchik') }}";
+                        case '47' :
+                            return "{{ __('Nizhniy Novgorod') }}";
+                        case '65' :
+                            return "{{ __('Novosibirsk') }}";
+                        case '66' :
+                            return "{{ __('Omsk') }}";
+                        case '10' :
+                            return "{{ __('Eagle') }}";
+                        case '48' :
+                            return "{{ __('Orenburg') }}";
+                        case '49' :
+                            return "{{ __('Penza') }}";
+                        case '50' :
+                            return "{{ __('Perm') }}";
+                        case '25' :
+                            return "{{ __('Pskov') }}";
+                        case '39' :
+                            return "{{ __('Rostov-on') }}";
+                        case '11' :
+                            return "{{ __('Ryazan') }}";
+                        case '51' :
+                            return "{{ __('Samara') }}";
+                        case '42' :
+                            return "{{ __('Saransk') }}";
+                        case '2' :
+                            return "{{ __('Saint-Petersburg') }}";
+                        case '12' :
+                            return "{{ __('Smolensk') }}";
+                        case '239' :
+                            return "{{ __('Sochi') }}";
+                        case '36' :
+                            return "{{ __('Stavropol') }}";
+                        case '973' :
+                            return "{{ __('Surgut') }}";
+                        case '13' :
+                            return "{{ __('Tambov') }}";
+                        case '14' :
+                            return "{{ __('Tver') }}";
+                        case '67' :
+                            return "{{ __('Tomsk') }}";
+                        case '15' :
+                            return "{{ __('Tula') }}";
+                        case '195' :
+                            return "{{ __('Ulyanovsk') }}";
+                        case '172' :
+                            return "{{ __('Ufa') }}";
+                        case '76' :
+                            return "{{ __('Khabarovsk') }}";
+                        case '45' :
+                            return "{{ __('Cheboksary') }}";
+                        case '56' :
+                            return "{{ __('Chelyabinsk') }}";
+                        case '1104' :
+                            return "{{ __('Cherkessk') }}";
+                        case '16' :
+                            return "{{ __('Yaroslavl') }}";
+                    }
+                }
 
                 $('#user_jobs_table').dataTable({
                     "order": [[0, "desc"]],
@@ -1075,7 +1075,7 @@
                                     0: '<td class="sorting_1">' +
                                         value['user']['email'] +
                                         '   <div class="text-muted">' +
-                                        value['user']['name'] +' ' +
+                                        value['user']['name'] + ' ' +
                                         value['user']['last_name'] +
                                         '   </div>' +
                                         '</td>',
