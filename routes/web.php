2325fb2e6b22f5e022402e9caaeb15ddc60b689d
Nikolay <tifilum74@mail.ru>
2023-03-20 13:01:06 +0300

    #160055: changes

--- routes/web.php
+++ routes/web.php
@@ -276,8 +276,9 @@ Route::middleware(['verified'])->group(function () {
     Route::post('monitoring/{id}/groups', 'MonitoringGroupsController@action')->name('groups.action');
 
     Route::resource('monitoring', 'MonitoringController');
-    Route::get('/monitoring/projects/{project}/competitors', 'MonitoringController@monitoringCompetitors')->name('monitoring.competitors');
+    Route::get('/monitoring/{project}/competitors', 'MonitoringController@monitoringCompetitors')->name('monitoring.competitors');
     Route::post('/monitoring/projects/competitors', 'MonitoringController@getCompetitorsInfo')->name('monitoring.get.competitors');
+    Route::get('/monitoring/{project}/competitors/positions', 'MonitoringController@competitorsPositions')->name('monitoring.competitors.positions');
     Route::post('/monitoring/add-competitor', 'MonitoringController@addCompetitor')->name('monitoring.add.competitor');
     Route::post('/monitoring/remove-competitor', 'MonitoringController@removeCompetitor')->name('monitoring.remove.competitor');
     Route::get('/monitoring/projects/get', 'MonitoringController@getProjects')->name('monitoring.projects.get');
