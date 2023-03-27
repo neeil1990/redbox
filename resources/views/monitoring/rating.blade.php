e83dcd106bb82dc513992eb03eea092eaf612748
Nikolay <tifilum74@mail.ru>
2023-03-27 10:02:14 +0300

    #160055: changes

--- resources/views/monitoring/rating.blade.php
+++ resources/views/monitoring/rating.blade.php
@@ -75,7 +75,30 @@
         </div>
     </div>
 
-    <div class="card direct-chat direct-chat-primary collapsed-card">
+    <h3 class="mt-3 mr-3">
+        {{  __('Project') . " $project->name" }}
+    </h3>
+
+    <h4>
+        {{ __('Count competitors') }}: {{ count($competitors) }}
+    </h4>
+
+
+    <div class="d-flex justify-content-center align-items-center align-content-center">
+        <img src="/img/1485.gif" style="width: 50px; height: 50px;" id="preloader">
+    </div>
+
+    <table id="table" class="table table-hover table-bordered no-footer" style="display: none">
+        <thead>
+        <tr id="tableHeadRow">
+            <th>{{ __('Query') }}</th>
+        </tr>
+        </thead>
+        <tbody id="tableBody">
+        </tbody>
+    </table>
+
+    <div class="card direct-chat direct-chat-primary collapsed-card mt-3">
         <div class="card-header ui-sortable-handle">
             <button id="more-info" class="btn btn-default"
                     data-card-widget="collapse">{{ __('Positions competitors') }}</button>
@@ -100,30 +123,28 @@
 
                 </tbody>
             </table>
+
+            <div class="card-footer d-flex flex-column">
+                <div class="d-flex flex-row mt-3">
+                    <div class="col-6">
+                        <canvas id="bar-chart"></canvas>
+                    </div>
+                    <div class="col-6">
+                        <canvas id="bar-chart-3"></canvas>
+                    </div>
+                </div>
+                <div class="d-flex flex-row mt-3">
+                    <div class="col-6">
+                        <canvas id="bar-chart-10"></canvas>
+                    </div>
+                    <div class="col-6">
+                        <canvas id="bar-chart-100"></canvas>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <h3 class="mt-3 mr-3">
-        {{  __('Project') . " $project->name" }}
-    </h3>
-
-    <h4>
-        {{ __('Count competitors') }}: {{ count($competitors) }}
-    </h4>
-
-    <div class="d-flex justify-content-center align-items-center align-content-center">
-        <img src="/img/1485.gif" style="width: 50px; height: 50px;" id="preloader">
-    </div>
-
-    <table id="table" class="table table-hover table-bordered no-footer" style="display: none">
-        <thead>
-        <tr id="tableHeadRow">
-            <th>{{ __('Query') }}</th>
-        </tr>
-        </thead>
-        <tbody id="tableBody">
-        </tbody>
-    </table>
 
     @slot('js')
         <!-- DataTables  & Plugins -->
@@ -133,6 +154,12 @@
         <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
         <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
         <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+
+        <!-- Charts -->
+        <script src="{{ asset('plugins/chart.js/3.9.1/chart.js') }}"></script>
+        <script src="{{ asset('plugins/chart.js/3.9.1/plugins/chartjs-plugin-crosshair.js') }}"></script>
+        <script src="{{ asset('plugins/chart.js/3.9.1/plugins/chartjs-plugin-datalabels.js') }}"></script>
+
         <script>
             let table
 
@@ -252,9 +279,10 @@
                                     iterator++
                                 })
 
-
                                 $('#preloader-competitors').hide()
                                 $('#position-table').show()
+
+                                renderCharts(response.data)
                             },
                         });
                     }
@@ -339,6 +367,169 @@
                 $('#preloader').hide()
 
                 return res;
+            }
+
+            function renderCharts(data) {
+                let colorArray = getColorArray()
+
+                let labels = []
+                let datas = []
+                let top3 = []
+                let top10 = []
+                let top100 = []
+                let colors = []
+
+                $.each(data, function (domain, info) {
+                    labels.push(domain)
+                    datas.push(info.avg)
+                    colors.push(colorArray.shift())
+                    top3.push(info.top_3)
+                    top10.push(info.top_10)
+                    top100.push(info.top_100)
+                })
+                new Chart(document.getElementById("bar-chart"), {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: "{{ __("Average position") }}",
+                                backgroundColor: colors,
+                                data: datas
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                suggestedMin: 0,
+                                suggestedMax: 100
+                            },
+                        },
+                    }
+                });
+                new Chart(document.getElementById("bar-chart-3"), {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: "{{ __("Top") }} 3",
+                                backgroundColor: colors,
+                                data: top3
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                suggestedMin: 0,
+                                suggestedMax: 100
+                            },
+                        },
+                    }
+                });
+                new Chart(document.getElementById("bar-chart-10"), {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: "{{ __("Top") }} 10",
+                                backgroundColor: colors,
+                                data: top10
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                suggestedMin: 0,
+                                suggestedMax: 100
+                            },
+                        },
+                    }
+                });
+                new Chart(document.getElementById("bar-chart-100"), {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [
+                            {
+                                label: "{{ __("Top") }} 100",
+                                backgroundColor: colors,
+                                data: top100
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                suggestedMin: 0,
+                                suggestedMax: 100
+                            },
+                        },
+                    }
+                });
+            }
+
+            function getColorArray() {
+                let colorArray = [
+                    "rgba(220, 51, 10, 0.6)",
+                    "rgb(203,60,25)",
+                    "rgba(121, 25, 6, 1)",
+                    "rgba(214, 96, 110, 0.6)",
+                    "rgba(214, 96, 110, 1)",
+                    "rgba(252, 170, 153, 0.6)",
+                    "rgba(252, 170, 153, 1)",
+                    "rgba(214, 2, 86, 0.6)",
+                    "rgba(214, 2, 86, 1)",
+                    "rgba(147,50,88, 1)",
+                    "rgba(247, 220, 163, 1)",
+                    "rgba(204, 118, 32, 0.6)",
+                    "rgba(204, 118, 32, 1)",
+                    "rgba(255,89,0,0.6)",
+                    "rgba(255, 89, 0, 1)",
+                    "rgba(164, 58 ,1, 1)",
+                    "rgba(73, 28, 1, 0.6)",
+                    "rgba(178, 135, 33, 0.6)",
+                    "rgba(178, 135, 33, 1)",
+                    "rgba(246, 223, 78, 1)",
+                    "rgba(1, 253, 215, 0.6)",
+                    "rgba(1, 253, 215, 1)",
+                    "rgba(1, 148, 130, 0.6)",
+                    "rgba(1, 79, 66, 0.6)",
+                    "rgba(139, 150, 24, 0.6)",
+                    "rgba(154, 205, 50, 0.6)",
+                    "rgba(154, 205, 50, 1)",
+                    "rgb(17, 255, 0)",
+                    "rgba(151, 186, 229, 1)",
+                    "rgba(0, 69, 255, 0.6)",
+                    "rgba(0, 69, 255, 1)",
+                    "rgba(1, 45, 152, 0.6)",
+                    "rgba(157, 149, 226, 1)",
+                    "rgba(6, 136, 165, 0.6)",
+                    "rgba(64, 97, 206, 1)",
+                    "rgba(19,212,224, 0.6)",
+                    "rgba(19,212,224, 1)",
+                    "rgba(2, 97, 214, 0.6)",
+                    "rgba(159, 112, 216, 0.6)",
+                    "rgba(239, 50, 223, 0.6)",
+                    "rgba(239, 50, 223, 1)",
+                    "rgba(209, 46, 127, 0.6)",
+                    "rgba(209, 46, 127, 1)",
+                    "rgba(194, 85, 237, 1)",
+                    "rgba(252, 194, 243, 1)",
+                    "rgba(244, 139, 200, 0.6)",
+                    "rgba(244, 139, 200, 1)",
+                    "rgba(87, 64, 64, 0.6)",
+                    "rgba(239, 211, 211, 0.6)",
+                    "rgba(163, 209, 234, 0.6)",
+                    "rgba(234,163,163,0.6)",
+                    "rgba(232,194,90,0.6)",
+                ]
+
+                return colorArray.sort(() => Math.random() - 0.5);
             }
         </script>
     @endslot
