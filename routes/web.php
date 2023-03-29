b607f5f9aef15ea5e3613071adba3386df3ddd4f
Nikolay <tifilum74@mail.ru>
2023-03-29 12:21:48 +0300

    #2034862: changes

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ use App\User;
 use Carbon\CarbonPeriod;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 Route::get('info', function () {
     phpinfo();
@@ -384,4 +385,67 @@ Route::middleware(['verified'])->group(function () {
     Route::get('/partners/admin', 'PartnersController@admin')->name('partners.admin');
     Route::post('/partners/edit-item/', 'PartnersController@editItem')->name('partners.save.edit.item');
     Route::get('/partners/r/{short_link}', 'PartnersController@redirect')->name('partners.redirect');
+});
+
+Route::get('/get-project/test', function () {
+
+    $project = MonitoringProject::findOrFail(77);
+
+    $engines = MonitoringSearchengine::where('id', '=', 118)->get(['lr', 'engine']);
+
+    $competitors = [];
+
+    foreach ($engines as $searchengine) {
+        foreach ($project->keywords as $keyword) {
+            $results = SearchIndex::where('lr', '=', $searchengine->lr)
+                ->where('query', '=', $keyword->query)
+                ->where('position', '<=', 10)
+                ->latest()
+                ->pluck('query', 'url');
+
+            foreach ($results as $url => $query) {
+                $host = parse_url(Common::domainFilter($url))['host'];
+                if (isset($request['targetDomain'])) {
+                    if ($host === $request['targetDomain']) {
+                        $competitors[$host]['urls'][$keyword->query][$searchengine->engine][] = Common::domainFilter($url);
+                    }
+                } else {
+                    $competitors[$host]['urls'][$searchengine->engine][$keyword->query][] = Common::domainFilter($url);
+                }
+            }
+        }
+
+    }
+
+    foreach ($project->competitors as $competitor) {
+        $url = Common::domainFilter($competitor->url);
+
+        if (array_key_exists($url, $competitors)) {
+            $competitors[$url]['competitor'] = true;
+        }
+    }
+
+    if (array_key_exists($project->url, $competitors)) {
+        $competitors[$project->url]['mainPage'] = true;
+    }
+
+    foreach ($competitors as $key => $urls) {
+        $count = 0;
+        foreach ($urls as $inf => $engines) {
+            if ($inf !== 'urls') {
+                continue;
+            }
+            foreach ($engines as $k => $words) {
+                foreach ($words as $k1 => $word) {
+                    $word = array_unique($word);
+                    $count += count($word);
+                    $competitors[$key][$inf][$k][$k1] = $word;
+                }
+            }
+        }
+
+        $competitors[$key]['visibility'] = $count;
+    }
+
+    return $competitors;
 });
