3afbe34fa7f8c5690d93481ef98efd7c142b88af
Nikolay <tifilum74@mail.ru>
2023-04-26 22:38:16 +0300

    #160660: update statistics

--- resources/views/monitoring/competitors/statistics.blade.php
+++ resources/views/monitoring/competitors/statistics.blade.php
@@ -111,7 +111,9 @@
                 <a href="{{ $navigation['href'] }}" class="small-box {{ $navigation['bg'] }}" style="min-height: 137px">
                     <div class="inner">
                         <h3>{{ $navigation['h3'] }}</h3>
-                        <p>{{ $navigation['p'] }}</p>
+                        @isset($navigation['p'])
+                            {{ $navigation['p'] }}
+                        @endisset
                         @isset($navigation['small'])
                             <small>{{ $navigation['small'] }}</small>
                         @endisset
