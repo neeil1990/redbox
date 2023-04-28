c631aa65d714549a6ae9ed105472fdabc548a10b
Nikolay <tifilum74@mail.ru>
2023-04-28 17:05:50 +0300

    #157623: updates

--- app/MonitoringCompetitor.php
+++ app/MonitoringCompetitor.php
@@ -137,11 +137,12 @@ class MonitoringCompetitor extends Model
             $competitorStatistics[$key]['top_3'] = Common::percentHitIn(3, $item['positions']);
             $competitorStatistics[$key]['top_10'] = Common::percentHitIn(10, $item['positions']);
             $competitorStatistics[$key]['top_100'] = Common::percentHitIn(100, $item['positions']);
+            $competitorStatistics[$key]['new_key'] = count($item['positions']);
         }
 
         return [
             'visibility' => $visibilityArray,
-            'statistics' => $competitorStatistics
+            'statistics' => $competitorStatistics,
         ];
     }
 }
