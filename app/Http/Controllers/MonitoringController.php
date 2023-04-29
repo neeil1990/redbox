dc80c5ee221679a44c9cfd7062f6ecc6da010838
Nikolay <tifilum74@mail.ru>
2023-04-29 18:33:51 +0300

    #157623: updates

--- app/Http/Controllers/MonitoringController.php
+++ app/Http/Controllers/MonitoringController.php
@@ -562,8 +562,7 @@ class MonitoringController extends Controller
         $records = [];
         foreach ($items as $keywords) {
             $start = microtime(true);
-            $results = SearchIndex::from(DB::raw('search_indexes USE INDEX (search_indices_query_index, search_indices_lr_index, search_indices_position_index)'))
-            ->whereBetween('created_at', [
+            $results = SearchIndex::whereBetween('created_at', [
                 date('Y-m-d H:i:s', strtotime($request->date . ' 00:00:00')),
                 date('Y-m-d H:i:s', strtotime($request->date . ' 23:59:59')),
             ])->where('lr', '=', $lr)
