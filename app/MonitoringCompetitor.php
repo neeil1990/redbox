3d9ebd9a84b542402202387cfdd705c73296bbbb
Nikolay <tifilum74@mail.ru>
2023-05-04 15:37:16 +0300

    #157623: optimization

--- app/MonitoringCompetitor.php
+++ app/MonitoringCompetitor.php
@@ -29,7 +29,7 @@ class MonitoringCompetitor extends Model
             $start = microtime(true);
             foreach ($words as $keywords) {
                 $results = DB::table(DB::raw('search_indices use index(search_indices_query_index, search_indices_lr_index, search_indices_position_index)'))
-                    ->where('created_at', '>=', Carbon::now()->subDays(15)->toDateString() . '00:00:00')
+                    ->where('created_at', '>=', Carbon::now()->subDays(15)->toDateString() . ' 00:00:00')
                     ->where('lr', $engine['lr'])
                     ->whereIn('query', $keywords)
                     ->where('position', '<=', 10)
