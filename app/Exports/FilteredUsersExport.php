617539b3acf57d28ebd6f9bcc5acd68ef548d436
Nikolay <tifilum74@mail.ru>
2023-06-01 11:13:21 +0300

    hot fix

--- app/Exports/FilteredUsersExport.php
+++ app/Exports/FilteredUsersExport.php
@@ -5,6 +5,7 @@ namespace App\Exports;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
 class FilteredUsersExport implements FromCollection
@@ -70,48 +71,48 @@ class FilteredUsersExport implements FromCollection
                 $user->last_online_at->format('d.m.Y'),
             ];
 
-            $metrics = $user->metrics;
-            if (isset($metrics)) {
-                if ($user->metrics !== null) {
-                    if (gettype($metrics) === 'string') {
-                        $arr = json_decode($metrics, true);
-                    }
+            if (isset($user->metrics)) {
+                if (gettype($user->metrics) === 'string') {
+                    $arr = json_decode($user->metrics, true);
+                } else {
+                    $arr = $user->metrics;
+                }
 
-                    if (isset($arr['utm_campaign'])) {
-                        $rows[$iterator][8] = urldecode($arr['utm_campaign']);
-                    } else {
-                        $rows[$iterator][8] = '';
-                    }
+                dd($arr);
+                if (isset($arr['utm_campaign'])) {
+                    $rows[$iterator][8] = urldecode($arr['utm_campaign']);
+                } else {
+                    $rows[$iterator][8] = '';
+                }
 
-                    if (isset($arr['utm_source'])) {
-                        $rows[$iterator][9] = urldecode($arr['utm_source']);
-                    } else {
-                        $rows[$iterator][9] = '';
-                    }
+                if (isset($arr['utm_source'])) {
+                    $rows[$iterator][9] = urldecode($arr['utm_source']);
+                } else {
+                    $rows[$iterator][9] = '';
+                }
 
-                    if (isset($arr['utm_medium'])) {
-                        $rows[$iterator][10] = urldecode($arr['utm_medium']);
-                    } else {
-                        $rows[$iterator][10] = '';
-                    }
+                if (isset($arr['utm_medium'])) {
+                    $rows[$iterator][10] = urldecode($arr['utm_medium']);
+                } else {
+                    $rows[$iterator][10] = '';
+                }
 
-                    if (isset($arr['utm_term_keyword'])) {
-                        $rows[$iterator][11] = urldecode($arr['utm_term_keyword']);
-                    } else {
-                        $rows[$iterator][11] = '';
-                    }
+                if (isset($arr['utm_term_keyword'])) {
+                    $rows[$iterator][11] = urldecode($arr['utm_term_keyword']);
+                } else {
+                    $rows[$iterator][11] = '';
+                }
 
-                    if (isset($arr['utm_term_source'])) {
-                        $rows[$iterator][12] = urldecode($arr['utm_term_source']);
-                    } else {
-                        $rows[$iterator][12] = '';
-                    }
+                if (isset($arr['utm_term_source'])) {
+                    $rows[$iterator][12] = urldecode($arr['utm_term_source']);
+                } else {
+                    $rows[$iterator][12] = '';
+                }
 
-                    if (isset($arr['utm_content'])) {
-                        $rows[$iterator][13] = urldecode($arr['utm_content']);
-                    } else {
-                        $rows[$iterator][13] = '';
-                    }
+                if (isset($arr['utm_content'])) {
+                    $rows[$iterator][13] = urldecode($arr['utm_content']);
+                } else {
+                    $rows[$iterator][13] = '';
                 }
             } else {
                 $rows[$iterator][8] = '';
