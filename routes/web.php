04b87a72b4129d2489234a812b339b1edfc7531a
Nikolay <tifilum74@mail.ru>
2023-07-09 13:18:36 +0300

    added click tracking

--- routes/web.php
+++ routes/web.php
@@ -398,3 +398,16 @@ Route::middleware(['verified'])->group(function () {
 
     Route::post('/click-tracking', 'HomeController@clickTracking')->name('click.tracking');
 });
+Route::get('/test', function () {
+    $usersIds = User::where('statistic', 1);
+    $usersIds = $usersIds->pluck('id')->toArray();
+
+    $records = ClickTracking::where('project_id', 1)
+        ->whereIn('user_id', $usersIds)
+        ->with('user')
+        ->get(['user_id', 'url', 'project_id', 'button_text', 'button_counter'])
+        ->groupBy('user.email');
+
+    dump($usersIds);
+    dd($records);
+});
