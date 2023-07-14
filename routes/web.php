99644b2175be7bdd962a323b6a47ad136b007e20
Nikolay <tifilum74@mail.ru>
2023-07-14 14:18:52 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -399,69 +399,8 @@ Route::middleware(['verified'])->group(function () {
 });
 
 Route::get('/system-load', function () {
-    function getServerLoad()
-    {
-        $load = null;
-
-        if (stristr(PHP_OS, "win"))
-        {
-            $cmd = "wmic cpu get loadpercentage /all";
-            @exec($cmd, $output);
-
-            if ($output)
-            {
-                foreach ($output as $line)
-                {
-                    if ($line && preg_match("/^[0-9]+\$/", $line))
-                    {
-                        $load = $line;
-                        break;
-                    }
-                }
-            }
-        }
-        else
-        {
-            if (is_readable("/proc/stat"))
-            {
-                // Collect 2 samples - each with 1 second period
-                // See: https://de.wikipedia.org/wiki/Load#Der_Load_Average_auf_Unix-Systemen
-                $statData1 = _getServerLoadLinuxData();
-                sleep(1);
-                $statData2 = _getServerLoadLinuxData();
-
-                if
-                (
-                    (!is_null($statData1)) &&
-                    (!is_null($statData2))
-                )
-                {
-                    // Get difference
-                    $statData2[0] -= $statData1[0];
-                    $statData2[1] -= $statData1[1];
-                    $statData2[2] -= $statData1[2];
-                    $statData2[3] -= $statData1[3];
-
-                    // Sum up the 4 values for User, Nice, System and Idle and calculate
-                    // the percentage of idle time (which is part of the 4 values!)
-                    $cpuTime = $statData2[0] + $statData2[1] + $statData2[2] + $statData2[3];
-
-                    // Invert percentage to get CPU time, not idle time
-                    $load = 100 - ($statData2[3] * 100 / $cpuTime);
-                }
-            }
-        }
-
-        return $load;
-    }
-
-    //----------------------------
-
-    $cpuLoad = getServerLoad();
-    if (is_null($cpuLoad)) {
-        echo "CPU load not estimateable (maybe too old Windows or missing rights at Linux or Windows)";
-    }
-    else {
-        echo $cpuLoad . "%";
-    }
+    $loads = sys_getloadavg();
+    $core_nums = trim(shell_exec("grep -P '^processor' /proc/cpuinfo|wc -l"));
+    $load = round($loads[0]/($core_nums + 1)*100, 2);
+    echo $load;
 });
