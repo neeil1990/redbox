e1300d110daa41248955fbef9a7ebfa43ef65928
neil <neeil@mail.ru>
2024-02-16 14:07:17 +0300

    fix bug

--- resources/views/monitoring/partials/_table.blade.php
+++ resources/views/monitoring/partials/_table.blade.php
@@ -1,6 +1,6 @@
 
 <div class="col-md-12">
-    <div class="card">
+    <div class="card table-card">
         <table class="table table-hover projects" id="projects" style="width:100%"></table>
     </div>
 </div>
