38fdbb7a427b2ed0140883e73dc635d8283b803c
Neil <neeil@mail.ru>
2025-01-30 19:59:53 +0300

    forceTLS

--- public/js/app.js
+++ public/js/app.js
@@ -96342,8 +96342,8 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: "pusher",
   key: "local",
   wsHost: window.location.hostname,
-  wsPort: 6001,
-  wssPort: 6001,
+  wsPort: 443,
+  wssPort: 443,
   forceTLS: true,
   disableStats: true,
   enabledTransports: ["ws", "wss"]
