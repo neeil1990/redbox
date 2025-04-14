3b94e4dae69b4c5b3b38648f332a5176eecf8cdf
Neil <neeil@mail.ru>
2025-04-14 13:05:28 +0300

    html bugfix

--- resources/views/monitoring/partials/show/url.blade.php
+++ resources/views/monitoring/partials/show/url.blade.php
@@ -1,4 +1,4 @@
-<a href="#" @if($urls->count()) data-toggle="popover" @endif title="URL" data-content="{{ view('monitoring.partials.show.popover.urls', ['urls' => $urls]) }}">
+<a href="#" @if($urls->count()) data-toggle="popover" @endif title="URL" data-content='{{ view('monitoring.partials.show.popover.urls', ['urls' => $urls]) }}'>
     <span class="badge badge-light">
         <i class="fas fa-link"></i>
         <span class="{{ $textClass }} text-sm text-bold">{{ $urls->count() }}</span>
