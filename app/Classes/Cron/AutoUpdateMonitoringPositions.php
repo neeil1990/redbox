acd39bf1a75654aafcc08fb78a586a602e4bd8c1
Neil <neeil@mail.ru>
2025-04-16 12:31:01 +0300

    auto update positions bugfix

--- app/Classes/Cron/AutoUpdateMonitoringPositions.php
+++ app/Classes/Cron/AutoUpdateMonitoringPositions.php
@@ -19,9 +19,8 @@ class AutoUpdateMonitoringPositions
     {
         $engine = $this->engine;
         $project = $engine->project;
-        $user = $project->admin->first();
 
-        $queue = new PositionsDispatch($user["id"], 'position_low');
+        $queue = new PositionsDispatch($project->creator, 'position_low');
         foreach ($project->keywords as $query)
             $queue->addQueryWithRegion($query, $engine);
 
