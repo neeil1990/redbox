135ebdab61653d6908bab9fd4fe4f1de967ab472
neeil1990 <neeil@mail.ru>
2026-02-13 14:13:11 +0300

    Update CroneController.php

--- app/Http/Controllers/CroneController.php
+++ app/Http/Controllers/CroneController.php
@@ -39,7 +39,6 @@ class CroneController extends Controller
     public function checkDomains()
     {
         $projects = DomainInformation::all();
-        $projects = $projects->where('domain', 'kawe.su')->where('user_id', 2);
 
         foreach ($projects as $project) {
             DomainInformation::checkDomain($project);
