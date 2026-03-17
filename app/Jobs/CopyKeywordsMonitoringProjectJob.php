beac312be0ec509a27225272b4ea65930dad6372
neeil1990 <neeil@mail.ru>
2026-03-17 14:42:46 +0300

    fix copy

---
+++ app/Jobs/CopyKeywordsMonitoringProjectJob.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\MonitoringProjectCopyProgress;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class CopyKeywordsMonitoringProjectJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $keyword;
+    public $keywordIds;
+    public $searchengineIds;
+
+    public $userId;
+
+    public $keywordsCount;
+
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($userId, $keyword, $keywordIds, $searchengineIds, $keywordsCount)
+    {
+        $this->userId = $userId;
+        $this->keyword = $keyword;
+        $this->keywordIds = $keywordIds;
+        $this->searchengineIds = $searchengineIds;
+        $this->keywordsCount = $keywordsCount;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        foreach ($this->keyword->positions as $position) {
+            $newPosition = $position->replicate();
+            $newPosition->monitoring_keyword_id = $this->keywordIds[$position->monitoring_keyword_id];
+            $newPosition->monitoring_searchengine_id = $this->searchengineIds[$position->monitoring_searchengine_id];
+            $newPosition->created_at = $position->created_at;
+            $newPosition->updated_at = $position->updated_at;
+            $newPosition->save();
+        }
+
+        foreach ($this->keyword->prices as $price) {
+            $newPrice = $price->replicate();
+            $newPrice->monitoring_keyword_id = $this->keywordIds[$price->monitoring_keyword_id];
+            $newPrice->monitoring_searchengine_id = $this->searchengineIds[$price->monitoring_searchengine_id];
+            $newPrice->save();
+        }
+
+        $this->sendProcess("Ключевых запросов осталось: " . $this->keywordsCount);
+    }
+
+    protected function sendProcess(string $message)
+    {
+        event(new MonitoringProjectCopyProgress($this->userId, $message));
+    }
+}
--- app/Jobs/MonitoringProjectCopyJob.php
+++ app/Jobs/MonitoringProjectCopyJob.php
@@ -51,8 +51,6 @@ class MonitoringProjectCopyJob implements ShouldQueue
             $groupIds[$groups->id] = $newGroup->id;
         }
 
-        $this->sendProcess("Группы скопированы");
-
         $keywordIds = [];
         foreach ($original->keywords as $keyword) {
             $newKeyword = $keyword->replicate();
@@ -62,8 +60,6 @@ class MonitoringProjectCopyJob implements ShouldQueue
             $keywordIds[$keyword->id] = $newKeyword->id;
         }
 
-        $this->sendProcess("Ключевые запросы скопированы");
-
         $searchengineIds = [];
         foreach ($original->searchengines as $engine) {
             $newEngine = $engine->replicate();
@@ -72,50 +68,19 @@ class MonitoringProjectCopyJob implements ShouldQueue
             $searchengineIds[$engine->id] = $newEngine->id;
         }
 
-        $this->sendProcess("Поисковые системы скопированы");
-
         $keywordsCount = $original->keywords()->count();
 
-        $this->sendProcess("Копирование цен и позиций ключевых запросов");
-        $this->sendProcess("Ключевых запросов осталось: " . $keywordsCount);
-
         foreach ($original->keywords as $keyword) {
-            foreach ($keyword->positions as $position) {
-                $newPosition = $position->replicate();
-                $newPosition->monitoring_keyword_id = $keywordIds[$position->monitoring_keyword_id];
-                $newPosition->monitoring_searchengine_id = $searchengineIds[$position->monitoring_searchengine_id];
-                $newPosition->created_at = $position->created_at;
-                $newPosition->updated_at = $position->updated_at;
-                $newPosition->save();
-            }
-
-            foreach ($keyword->prices as $price) {
-                $newPrice = $price->replicate();
-                $newPrice->monitoring_keyword_id = $keywordIds[$price->monitoring_keyword_id];
-                $newPrice->monitoring_searchengine_id = $searchengineIds[$price->monitoring_searchengine_id];
-                $newPrice->save();
-            }
+            dispatch(new CopyKeywordsMonitoringProjectJob($this->userId, $keyword, $keywordIds, $searchengineIds, $keywordsCount));
 
             $keywordsCount -= 1;
-
-            $this->sendProcess("Ключевых запросов осталось: " . $keywordsCount);
         }
-
-        $this->sendProcess("Позиции и цены скопированы");
 
         foreach ($original->competitors as $competitor) {
             $newCompetitor = $competitor->replicate();
             $newCompetitor->monitoring_project_id = $newProject->id;
             $newCompetitor->save();
         }
-
-        $this->sendProcess("Конкуренты скопированы");
-
-        $this->sendProcess("Копирование завершено (<a href='/monitoring/$newProject->id' target='_blank'>Перейти</a>)");
     }
 
-    protected function sendProcess(string $message)
-    {
-        event(new MonitoringProjectCopyProgress($this->userId, $message));
-    }
 }
